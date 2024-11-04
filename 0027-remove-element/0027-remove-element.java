91557393fc8d43c1fe6fcdd8c4f5d7b7506facd1
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-03 22:25:23 -0800

    Time: 0 ms (100%), Space: 41.9 MB (48.99%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,38 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0;
+        int index = 0;
 
-        int i = 0;
-        int j = nums.length - 1;
-
-        while(j >= i)
+        for(int i = 0 ; i < nums.length ; i++)
         {
-            // System.out.print(Arrays.toString(nums));
-            // System.out.println("i = " + i + "," + "j = " + j + ", " + "k = " + k);
             if(nums[i] != val)
             {
-                i++;
-                k++;
-            }
-            else
-            {
-                if(nums[j] != val)
-                {
-                    k++;
-
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    j--;
-                    i++;
-                }
-                else
-                {
-                    j--;
-                }
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return k;
-        
+        return index;
     }
 }
