26660aa8ea08b525bede6a0d6a9d9057b66ae129
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-06 18:19:12 -0800

    Time: 0 ms (100%), Space: 43.1 MB (59.57%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -21,8 +21,9 @@ class Solution {
     private TreeNode helper(int[] nums, int left, int right)
     {
         if(left > right) return null;
+
         int mid = left + (right - left) / 2;
-        TreeNode root = new TreeNode(nums[mid]); 
+        TreeNode root = new TreeNode(nums[mid]);
         root.left = helper(nums, left, mid - 1);
         root.right = helper(nums, mid + 1, right);
         return root;
