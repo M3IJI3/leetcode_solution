24abef760aaf5ee88ff80f733af45f0a77ca0e2d
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-11 15:49:12 -0800

    Time: 2 ms (78.77%), Space: 53.3 MB (22.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // int majority = 0;
+        // int res = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
-        int majority = 0;
+        // for(int n: nums)
+        // {
+        // map.put(n, map.getOrDefault(0, n) + 1);
+
+        // if(map.get(n) > majority)
+        // {
+        // res = n;
+        // majority = map.get(n);
+        // }
+        // }
+
+        // return res;
+        // --------------------------
+        // | MOORE VOTING ALGORITHM |
+        // --------------------------
+
         int res = 0;
+        int majority = 0;
 
-        for(int n: nums)
-        {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        for (int n : nums) {
+            if (majority == 0)  // 如果majority为0, 可以
+                res = n;        // 重新选择个数字进行"投票"
 
-            if(map.get(n) > majority)
-            {
-                res = n;
-                majority = map.get(n);
-            }
+            majority += n == res ? 1 : -1; // 如果后续遍历数字与前一轮数字res一致, 则"票数"加1. 反之减1
         }
 
         return res;
