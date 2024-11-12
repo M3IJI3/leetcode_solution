455c5b51ed77fa2a0753497cce0ded875688ded0
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-11 16:03:07 -0800

    Time: 0 ms (100%), Space: 57.6 MB (14.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,22 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         // k = k % nums.length;
+        // int[] temp = new int[nums.length];
 
-        // if(k != 0)
-        // {
-        //     int[] array = new int[nums.length];
-        //     System.arraycopy(nums, nums.length - k, array, 0, k);
-        //     System.arraycopy(nums, 0, array, k, nums.length - k);
-        //     System.arraycopy(array, 0, nums, 0, nums.length);
-        // }
+        // System.arraycopy(nums, nums.length - k, temp, 0, k);
+        // System.arraycopy(nums, 0, temp, k, nums.length - k);
+        // System.arraycopy(temp, 0, nums, 0, nums.length);
 
-        int n = nums.length;
-        k = k % n;
-
-        reverse(nums, 0, n - 1); // 先反转整个数组
-        reverse(nums, 0, k - 1); // 再反转  (0, k-1)
-        reverse(nums, k, n - 1); // 最后反转(k, n-1)
-
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length - 1);
     }
 
     private void reverse(int[] nums, int left, int right)
@@ -26,7 +20,7 @@ class Solution {
             int temp = nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
-
+            
             left++;
             right--;
         }
