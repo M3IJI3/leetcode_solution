47f16bac99895c68031300e3324d0a4b8d62b4fe
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-13 15:58:24 -0800

    Time: 2 ms (80.43%), Space: 45.4 MB (84.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean canJump(int[] nums) {
         int dest = nums.length - 1;
 
-        for(int i = nums.length - 1 ; i > 0 ; i--)
+        for(int i = nums.length - 2 ; i >= 0 ; i--)
         {
-            if(nums[i - 1] + (i - 1) >= dest)
+            if(nums[i] + (i) >= dest)
             {
-                dest = i - 1;
+                dest = i;
             }
         }
 
