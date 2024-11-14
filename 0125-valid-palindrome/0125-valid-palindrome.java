990c3ff2c31b6869f8c4b5ebd2c57b8a0a789dab
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-13 23:54:18 -0800

    Time: 3 ms (72.36%), Space: 42.9 MB (75.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,59 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").replaceAll("\\s+", "").trim().toLowerCase();
-        System.out.println(s);
+        // s = s.replaceAll("[^a-zA-Z0-9]", "").replaceAll("\\s+", "").trim().toLowerCase();
+
+        // int left = 0;
+        // int right = s.length() - 1;
+
+        // while(left < right)
+        // {
+        //     if(s.charAt(left) != s.charAt(right))
+        //     {
+        //         return false;
+        //     }
+
+        //     left++;
+        //     right--;
+        // }
+
+        // return true;
+
+        // --------------------------------------------------------
+
+        // s = s.replaceAll("[^a-zA-Z0-9]", "").replaceAll("\\s+", "").trim().toLowerCase();
+        // StringBuilder sb = new StringBuilder(s);
+        // return sb.reverse().toString().equals(s);
+
+        // --------------------------------------------------------
+
+        s = s.toLowerCase();
 
         int left = 0;
         int right = s.length() - 1;
 
         while(left < right)
         {
+            if(!(s.charAt(left) >= 'a' && s.charAt(left) <= 'z') && !(s.charAt(left) >= '0' && s.charAt(left) <= '9'))
+            {
+                left++;
+                continue;  //continue在这里可以跳过当前循环的剩余代码, 直接执行下一次循环
+            }
+
+            if(!(s.charAt(right) >= 'a' && s.charAt(right) <= 'z') && !(s.charAt(right) >= '0' && s.charAt(right) <= '9'))
+            {
+                right--;
+                continue;
+            }
+
             if(s.charAt(left) != s.charAt(right))
             {
                 return false;
             }
-
+            
             left++;
             right--;
         }
-
+        
         return true;
     }
 }
