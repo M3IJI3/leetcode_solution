b8daa4be96f60374e84bc8f43461bc932537a082
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-13 16:29:35 -0800

    Time: 1 ms (99.16%), Space: 45.1 MB (45.27%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,20 @@
 class Solution {
     public int jump(int[] nums) {
-        int near = 0; // 表示靠近跳跃位置的第一个点
-        int far  = 0; // 表示能跳到的最远位置
-        int jump = 0; // 跳跃次数
+        int jump = 0;
+        int near = 0;
+        int far = 0;
 
-        while(far < nums.length - 1) // 条件满足跳跃的最远位置不会超过数组长度
-        {
+        while (far < nums.length - 1) {
             int maxDistance = 0;
-            for(int i = near ; i <= far ; i++) // 遍历能跳到的所有点
-            {
-                // 保存能跳到的最远位置
+            for (int i = near; i <= far; i++) {
                 maxDistance = Math.max(maxDistance, i + nums[i]);
             }
-            
-            near = far + 1;    // 更新起点
-            far = maxDistance; // 更新能跳到的最远位置
+
+            near = far + 1;
+            far = maxDistance;
             jump++;
         }
+
         return jump;
     }
 }
