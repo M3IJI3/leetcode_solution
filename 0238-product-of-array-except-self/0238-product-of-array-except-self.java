dd8479f111aad56dc224db8a42027d6a9204010b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-13 17:07:53 -0800

    Time: 3 ms (24.85%), Space: 55.1 MB (78.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        // 初始化一个全为1的数组
         int[] answer = new int[nums.length];
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            answer[i] = 1;
-        }
+        Arrays.fill(answer, 1);
 
+        // 先把每个元素左边的乘积得出并存储
         int left = 1;
         for(int i = 0 ; i < nums.length ; i++)
         {
@@ -13,8 +12,9 @@ class Solution {
             left *= nums[i];
         }
 
+        // 再把每个元素右边的乘积得出并存储
         int right = 1;
-        for(int i = nums.length - 1 ; i >= 0; i--)
+        for(int i = nums.length - 1 ; i >= 0 ; i--)
         {
             answer[i] *= right;
             right *= nums[i];
