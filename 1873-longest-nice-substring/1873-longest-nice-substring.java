82924d376f0e984162874abada300e7af9afd6a9
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-19 00:10:32 -0800

    Time: 6 ms (47.04%), Space: 41.4 MB (94.71%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.java
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -3,16 +3,23 @@ class Solution {
         int n = s.length();
         int maxPos = 0;
         int maxLen = 0;
-        for (int i = 0; i < n; i++) {
+
+        for(int i = 0 ; i < n ; i++)
+        {
             int lower = 0;
             int upper = 0;
-            for (int j = i; j < n; j++) {
-                if (Character.isLowerCase(s.charAt(j))) {
-                    lower |= 1 << (s.charAt(j) - 'a');
-                } else {
-                    upper |= 1 << (s.charAt(j) - 'A');
+            for(int j = i ; j < n ; j++)
+            {
+                if(Character.isLowerCase(s.charAt(j)))
+                {
+                    lower |= 1 << (s.charAt(j) - 'a'); //计算出对于'a'的偏移量
                 }
-                if (lower == upper && j - i + 1 > maxLen) {
+                else
+                {
+                    upper |= 1 << (s.charAt(j) - 'A'); //计算出对于'A'的偏移量
+                }
+                if(lower == upper && j - i + 1 > maxLen)
+                {
                     maxPos = i;
                     maxLen = j - i + 1;
                 }
