ec43ccec613392325230fd33ee4c238341852988
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-19 02:09:31 -0800

    Time: 1 ms (48.21%), Space: 40.1 MB (94.49%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int divisorSubstrings(int num, int k) {
-        String temp = num + "";
+        String str = String.valueOf(num);
         int count = 0;
 
-        for(int i = 0 ; i <= temp.length() - k ; i++)
+        for(int i = 0 ; i <= str.length() - k ; i++)
         {
-            int divisor = Integer.parseInt(temp.substring(i, i + k));
+            int divisor = Integer.parseInt(str.substring(i, i + k));
 
             if(divisor != 0 && num % divisor == 0)
             {
@@ -13,5 +13,18 @@ class Solution {
             }
         }
         return count;
+
+        // String str = String.valueOf(num);
+        // int n = str.length();
+        // int r=0;
+        // int count = 0;
+        // while(r+k <= n)
+        // {
+        //     int val = Integer.parseInt(str.substring(r,r+k));
+        //     if(val!=0 && num%val==0)
+        //         count++;
+        //     r++;
+        // }
+        // return count;
     }
 }
