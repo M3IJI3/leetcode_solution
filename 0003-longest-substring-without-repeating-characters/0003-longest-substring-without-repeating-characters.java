4e156725e1941e403f81ec41e95433d7262c004e
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-18 21:45:09 -0800

    Time: 7 ms (39.17%), Space: 44.2 MB (79.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,39 +1,39 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // int left = 0;
-        // int maxLen = 0;
-        // HashSet<Character> set = new HashSet<>(); // hashset 具有数值的唯一性
-
-        // for (int right = 0; right < s.length(); right++) {
-        //     while (set.contains(s.charAt(right))) {
-        //         set.remove(s.charAt(left));
-        //         left++;
-        //     }
-        //     set.add(s.charAt(right));
-        //     maxLen = Math.max(maxLen, right - left + 1);
-        // }
-        // return maxLen;
-
-        // ----------------------------------------------------
-
         int left = 0;
         int maxLen = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>(); // hashset 具有数值的唯一性
 
-        for(int right = 0 ; right < s.length() ; right++)
-        {
-            char c = s.charAt(right);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-
-            while(map.get(c) > 1)
-            {
-                char leftChar = s.charAt(left);
-                map.put(leftChar, map.get(leftChar) - 1);
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
                 left++;
             }
-
+            set.add(s.charAt(right));
             maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
+
+        // ----------------------------------------------------
+
+        // int left = 0;
+        // int maxLen = 0;
+        // Map<Character, Integer> map = new HashMap<>();
+
+        // for(int right = 0 ; right < s.length() ; right++)
+        // {
+        //     char c = s.charAt(right);
+        //     map.put(c, map.getOrDefault(c, 0) + 1);
+
+        //     while(map.get(c) > 1)
+        //     {
+        //         char leftChar = s.charAt(left);
+        //         map.put(leftChar, map.get(leftChar) - 1);
+        //         left++;
+        //     }
+
+        //     maxLen = Math.max(maxLen, right - left + 1);
+        // }
+        // return maxLen;
     }
 }
