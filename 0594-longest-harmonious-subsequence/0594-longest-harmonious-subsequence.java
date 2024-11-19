b34be4ad2ac67f345e7c949f6288ff4810d0c52b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-18 22:46:30 -0800

    Time: 17 ms (40.29%), Space: 45.5 MB (48.39%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,5 +1,29 @@
 class Solution {
     public int findLHS(int[] nums) {
+        // if (nums.length == 1)
+        //     return 0;
+
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for (int n : nums) {
+        //     map.put(n, map.getOrDefault(n, 0) + 1);
+        // }
+
+        // int maxLen = 0;
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (map.containsKey(nums[i] - 1)) {
+        //         maxLen = Math.max(maxLen, map.get(nums[i]) + map.get(nums[i] - 1));
+        //     }
+
+        //     if (map.containsKey(nums[i] + 1)) {
+        //         maxLen = Math.max(maxLen, map.get(nums[i]) + map.get(nums[i] + 1));
+        //     }
+        // }
+
+        // return maxLen;
+
+        // -----------------------------------------------
+
         if(nums.length == 1) return 0;
 
         Map<Integer, Integer> map = new HashMap<>();
@@ -10,19 +34,13 @@ class Solution {
         }
 
         int maxLen = 0;
-        for(int i = 0 ; i < nums.length - 1 ; i++)
+        for(int key: map.keySet())
         {
-            if(map.containsKey(nums[i] - 1))
+            if(map.containsKey(key) && map.containsKey(key + 1))
             {
-                maxLen = Math.max(maxLen, map.get(nums[i]) + map.get(nums[i] - 1));
-            }
-            
-            if(map.containsKey(nums[i] + 1))
-            {
-                maxLen = Math.max(maxLen, map.get(nums[i]) + map.get(nums[i] + 1));
+                maxLen = Math.max(maxLen, map.get(key) + map.get(key + 1));
             }
         }
-
         return maxLen;
     }
 }
