0d8b2da9718180334505820bc13339e4878e29bb
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-19 02:44:02 -0800

    Time: 1 ms (90.84%), Space: 41.4 MB (60.64%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,30 +1,58 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int left = 0;
-        int right = left + k - 1;
-        int min = Integer.MAX_VALUE;
-        HashMap<Character, Integer> map = new HashMap<>();
+        // int left = 0;
+        // int right = left + k - 1;
+        // int min = Integer.MAX_VALUE;
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        while(right < blocks.length())
+        // while(right < blocks.length())
+        // {
+        //     for(int i = left ; i <= right ; i++)
+        //     {
+        //         map.put(blocks.charAt(i), map.getOrDefault(blocks.charAt(i), 0) + 1);   
+        //     }
+            
+        //     if(!map.containsKey('W'))
+        //     {
+        //         return 0;
+        //     }
+        //     else
+        //     {
+        //         min = Math.min(min, map.get('W'));
+        //     }
+        //     map.clear();
+        //     left++;
+        //     right++;
+            
+        // }
+        // return min;
+
+        // -----------------------------------------------------
+
+        int ans = Integer.MAX_VALUE;
+        int n = blocks.length();
+        int count = 0;
+        for(int i = 0 ; i < k ; i++)
         {
-            for(int i = left ; i <= right ; i++)
+            if(blocks.charAt(i) == 'W')
             {
-                map.put(blocks.charAt(i), map.getOrDefault(blocks.charAt(i), 0) + 1);   
+                count++;
             }
-            
-            if(!map.containsKey('W'))
-            {
-                return 0;
-            }
-            else
-            {
-                min = Math.min(min, map.get('W'));
-            }
-            map.clear();
-            left++;
-            right++;
-            
         }
-        return min;
+        ans = Math.min(count, ans);
+        for(int i = k ; i < n ; i++)
+        {
+            int ws = i - k;
+            if(blocks.charAt(ws) == 'W')
+            {
+                count--;
+            }
+            if(blocks.charAt(i) == 'W')
+            {
+                count++;
+            }
+            ans = Math.min(count, ans);
+        }
+        return ans;
     }
 }
