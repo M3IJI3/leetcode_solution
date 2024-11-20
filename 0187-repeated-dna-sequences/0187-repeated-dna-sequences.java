0cece00c7fcc01477bcf6a6cb7dae30f1eb9af0f
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-19 16:34:37 -0800

    Time: 13 ms (97.69%), Space: 53.4 MB (42%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,25 +1,50 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans = new ArrayList<>();
-        String currentStr = "";
+        // List<String> ans = new ArrayList<>();
+        // String currentStr = "";
 
-        HashMap<String, Integer> map = new HashMap<>();
+        // HashMap<String, Integer> map = new HashMap<>();
 
-        for(int i = 0 ; i <= s.length() - 10; i++)
+        // for (int i = 0; i <= s.length() - 10; i++) {
+        // currentStr = s.substring(i, i + 10);
+        // map.put(currentStr, map.getOrDefault(currentStr, 0) + 1);
+        // }
+
+        // for (String key : map.keySet()) {
+        // if (map.get(key) > 1) {
+        // ans.add(key);
+        // }
+        // }
+
+        // return ans;
+
+        // ----------------------------------------------------
+
+        Set seen = new HashSet(), repeated = new HashSet();
+        int n = s.length();
+
+        for(int i = 0 ; i <= n - 10 ; i++)
         {
-            currentStr = s.substring(i, i + 10);
-            map.put(currentStr, map.getOrDefault(currentStr, 0) + 1);
-            currentStr = "";
-        }
-
-        for(String key : map.keySet())
+        String ten = s.substring(i, i + 10);
+        if(!seen.add(ten))
         {
-            if(map.get(key) > 1)
-            {
-                ans.add(key);
-            }
+        repeated.add(ten);
         }
+        }
+        return new ArrayList(repeated);
 
-        return ans;
+        // -----------------------------------------------------
+
+        // Set set = new HashSet(), res = new HashSet();
+        // int n = s.length();
+        // if (n < 10 || n > 10000) {
+        //     return new ArrayList(res);
+        // }
+        // for (int i = 0; i + 9 < n; i++) {
+        //     String ten = s.substring(i, i + 10);
+        //     if (!set.add(ten))
+        //         res.add(ten);
+        // }
+        // return new ArrayList(res);
     }
 }
