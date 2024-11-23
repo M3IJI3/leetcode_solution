1ebc040a2ac9730a9e2896c05a9ea1056d6da45f
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-22 17:44:14 -0800

    Time: 1 ms (100%), Space: 47.8 MB (43.21%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,21 +1,43 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int sum = 0;
-        int ans = 0;
+        // BRUTE FORCE : O(n^2)
+        // RUNNING TIME: 1862ms
+        // int sum = 0;
+        // int ans = 0;
 
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            for(int j = i ; j < nums.length ; j++)
-            {
-                sum += nums[j];
-                if(sum == goal)
-                {
-                    ans++;
-                }
+        // for(int i = 0 ; i < nums.length ; i++)
+        // {
+        // for(int j = i ; j < nums.length ; j++)
+        // {
+        // sum += nums[j];
+        // if(sum == goal)
+        // {
+        // ans++;
+        // }
+        // }
+        // sum = 0;
+        // }
+
+        // return ans;
+
+        // ---------------------------------------------
+
+        // SLIDING WINDOW
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+
+    }
+
+    private int atMost(int[] nums, int goal) {
+        if (goal < 0)
+            return 0; 
+        int left = 0, sum = 0, count = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum > goal) {
+                sum -= nums[left++];
             }
-            sum = 0;
+            count += right - left + 1;
         }
-
-        return ans;
+        return count;
     }
 }
