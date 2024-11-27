c89b5c75855cb6a5e0aaf73596fc376745c0e298
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-27 02:01:25 -0800

    Time: 3 ms (100%), Space: 53.2 MB (90.5%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,27 +1,25 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int count = 0; // 记录优美子数组的数量
-        int oddCount = 0; // 窗口内奇数的数量
-        int left = 0;
+        int n = nums.length;
+        int[] count = new int[n + 1];
+        count[0] = 1; // 初始化为1，处理边界情况
+        
+        int oddCount = 0; // 记录奇数个数
         int result = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] % 2 == 1) {
-                oddCount++;
-                count = 0;
+        
+        for (int num : nums) {
+            // 统计奇数个数
+            oddCount += num % 2;
+            
+            // 如果窗口内奇数个数大于等于k，则计算优美子数组数量
+            if (oddCount >= k) {
+                result += count[oddCount - k];
             }
-
-            while (oddCount == k) {
-                if (nums[left] % 2 == 1) {
-                    oddCount--;
-                }
-                left++;
-                count++;
-            }
-
-            result += count;
+            
+            // 记录当前奇数个数出现的频次
+            count[oddCount]++;
         }
+        
         return result;
-
     }
 }
