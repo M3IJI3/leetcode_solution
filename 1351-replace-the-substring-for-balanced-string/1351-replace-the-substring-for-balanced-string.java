fc5b26e84ca948dc0370a61af87b2bcd27def0eb
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-11-27 00:36:19 -0800

    Time: 8 ms (86.47%), Space: 43.5 MB (79.92%) - LeetHub

--- 1351-replace-the-substring-for-balanced-string/1351-replace-the-substring-for-balanced-string.java
+++ 1351-replace-the-substring-for-balanced-string/1351-replace-the-substring-for-balanced-string.java
@@ -59,34 +59,49 @@ class Solution {
         // count.getOrDefault('R', 0) <= target;
         // }
         // ----------------------------------------------
-        Map<Character, Integer> freq = new HashMap<>();
-        freq.put('Q', 0);
-        freq.put('W', 0);
-        freq.put('E', 0);
-        freq.put('R', 0);
+        // Map<Character, Integer> freq = new HashMap<>();
+        // freq.put('Q', 0);
+        // freq.put('W', 0);
+        // freq.put('E', 0);
+        // freq.put('R', 0);
 
-        for (int i = 0; i < s.length(); i++) {
-            freq.put(s.charAt(i), freq.get(s.charAt(i)) + 1);
+        // for (int i = 0; i < s.length(); i++) {
+        // freq.put(s.charAt(i), freq.get(s.charAt(i)) + 1);
+        // }
+
+        // int target = s.length() / 4;
+        // int minLen = s.length();
+        // int left = 0;
+
+        // for (int right = 0 ; right < s.length() ; right++) {
+        // freq.put(s.charAt(right), freq.get(s.charAt(right)) - 1);
+
+        // while (left < s.length() &&
+        // freq.get('Q') <= target &&
+        // freq.get('W') <= target &&
+        // freq.get('E') <= target &&
+        // freq.get('R') <= target) {
+        // minLen = Math.min(minLen, right - left + 1);
+        // freq.put(s.charAt(left), freq.get(s.charAt(left)) + 1);
+        // left++;
+        // }
+        // }
+
+        // return minLen;
+
+        // ----------------------------------------------
+        int[] count = new int[128];
+        int n = s.length(), res = n, i = 0, k = n / 4;
+        for (int j = 0; j < n; j++) {
+            count[s.charAt(j)]++;
         }
-
-        int target = s.length() / 4;
-        int minLen = s.length();
-        int left = 0;
-
-        for (int right = 0 ; right < s.length() ; right++) {
-            freq.put(s.charAt(right), freq.get(s.charAt(right)) - 1);
-
-            while (left < s.length() &&
-                    freq.get('Q') <= target &&
-                    freq.get('W') <= target &&
-                    freq.get('E') <= target &&
-                    freq.get('R') <= target) {
-                minLen = Math.min(minLen, right - left + 1);
-                freq.put(s.charAt(left), freq.get(s.charAt(left)) + 1);
-                left++;
+        for (int j = 0; j < n; j++) {
+            count[s.charAt(j)]--;
+            while (i < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k) {
+                res = Math.min(res, j - i + 1);
+                count[s.charAt(i++)]++;
             }
         }
-
-        return minLen;
+        return res;
     }
 }
