b24af667a8946c6a318b9f1123bc88cc7c5a8d5b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-12-06 18:13:28 -0800

    Time: 0 ms (100%), Space: 42.2 MB (6.68%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while(top <= bottom && left <= right)
+        {
+            // 遍历top行
+            for(int j = left ; j <= right ; j++)
+            {
+                result.add(matrix[top][j]);
+            }
+            top++;
+
+            // 遍历right列
+            for(int i = top; i <= bottom ; i++)
+            {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            // 遍历bottom行 (if valid)
+            if(top <= bottom)
+            {
+                for(int j = right ; j >= left ; j--)
+                {
+                    result.add(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            if(left <= right)
+            {
+                for(int i = bottom ; i >= top ; i--)
+                {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
