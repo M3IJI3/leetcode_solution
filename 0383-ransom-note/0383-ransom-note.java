56263c685cc9170a8c18d3ec8ef1941bd45269fb
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2024-12-13 16:19:53 -0800

    Time: 1 ms (99.53%), Space: 44.9 MB (56.47%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,22 +1,43 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
+
+        // for(char c: magazine.toCharArray())
+        // {
+        //     map.put(c, map.getOrDefault(c, 0) + 1);
+        // }
+
+        // for(char c: ransomNote.toCharArray())
+        // {
+        //     if(map.containsKey(c) && map.get(c) > 0)
+        //     {
+        //         map.put(c, map.get(c) - 1);
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        // ---------------------------------------------------------
+
+        short[] arr = new short[26];
 
         for(char c: magazine.toCharArray())
         {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            arr[c - 'a']++;
         }
 
         for(char c: ransomNote.toCharArray())
         {
-            if(map.containsKey(c) && map.get(c) > 0)
-            {
-                map.put(c, map.get(c) - 1);
-            }
-            else
+            if(arr[c - 'a'] <= 0)
             {
                 return false;
             }
+
+            arr[c - 'a']--;
         }
 
         return true;
