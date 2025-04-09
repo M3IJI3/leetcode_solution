7ccca134f4265bc274baeea9d243bf70943011da
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-08 20:08:32 -0700

    Time: 0 ms (100%), Space: 23.9 MB (50.27%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // int nonZeroPos = 0;
+        
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     if(nums[i] != 0){
+        //         nums[nonZeroPos] = nums[i];
+        //         nonZeroPos++;
+        //     }
+        // }
+
+        // for(int i = nonZeroPos ; i < nums.size() ; i++){
+        //     nums[i] = 0;
+        // }
+        ///////////////////////////////////////
+
+        int nonZeroPos = 0;
+
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] != 0){
+                if(i != nonZeroPos){
+                    nums[nonZeroPos] = nums[i];
+                    nums[i] = 0;
+                }
+                nonZeroPos++;
+            }
+        }
+    }
+};
