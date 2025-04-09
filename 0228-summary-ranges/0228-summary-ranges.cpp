98872a8aa77195c143ea33a77b20fcaf6c19b6e2
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-08 18:25:30 -0700

    Time: 0 ms (100%), Space: 9.2 MB (41.37%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        // vector<string> ans;
+        // int left = 0;
+        // int right = 1;
+
+        // for(int i  = 0 ; i < nums.size() - 1 ; i++){
+        //     if(nums[right] - nums[i] == 1){
+        //         right += 1;
+        //     } else {
+        //         std::string str = std::to_string(nums[left]) + "->" + std::to_string(nums[right - 1]);
+        //         left = right;
+        //         ans.push_back(str);
+        //     }
+        // }
+        // return ans;
+        ////////////////////////////////////////////
+
+        vector<string> result;
+        if(nums.empty()) return result;
+
+        int start = nums[0];
+        for(int i = 0 ; i < nums.size() ; i++){
+            // 1. check if the index 'i' reach the last element
+            // 2. check if the next element is consecutive, by subtracting the adjacent elements
+            if(i == nums.size() - 1 || nums[i] + 1 != nums[i + 1]){
+                // Check if the start element is same with the current one
+                // if it does,
+                if(start == nums[i]){
+                    result.push_back(std::to_string(start));
+                } 
+                // if it doesn't,
+                else {
+                    result.push_back(std::to_string(start) + "->" + std::to_string(nums[i]));
+                }
+
+                // update 'start' if i haven't reach to last
+                if(i < nums.size() - 1){
+                    start = nums[i + 1];
+                }
+            }
+        }
+        return result;
+    }
+};
