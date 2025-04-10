e58996100145db09ea68f05da07546f40ec09044
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-10 10:41:21 -0700

    Time: 41 ms (87.76%), Space: 87.9 MB (84.36%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        // types of candies
-        unordered_set<int> types(candyType.begin(), candyType.end());
+        // // types of candies
+        // unordered_set<int> types(candyType.begin(), candyType.end());
 
-        // numbers of candies that alice can eat
-        int target = candyType.size() / 2;
+        // // numbers of candies that alice can eat
+        // int target = candyType.size() / 2;
 
-        if(types.size() >= target){
-            return target;
-        } 
+        // if(types.size() >= target){
+        //     return target;
+        // } 
 
-        return types.size();
+        // return types.size();
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        sort(candyType.begin(), candyType.end());
+        stack<int> stk;
+        stk.push(candyType[0]);
+        int target = (candyType.size() / 2) - 1;
+
+        for(int i = 1 ; i < candyType.size() ; i++){
+            if(target > 0 && candyType[i] != stk.top()){
+                stk.push(candyType[i]);
+                target--;
+            }
+        }
+
+        return stk.size();
     }
 };
