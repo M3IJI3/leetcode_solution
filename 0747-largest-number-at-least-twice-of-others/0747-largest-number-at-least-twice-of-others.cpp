720a25c2fc75cff077f69fbdb63ca14f246d9564
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-10 18:40:37 -0700

    Time: 0 ms (100%), Space: 13.9 MB (48.13%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        int maxNum = INT_MIN;
-        int maxNumIndex = 0;
+        // int maxNum = INT_MIN;
+        // int maxNumIndex = 0;
 
-        for(int num: nums){
-            maxNum = max(maxNum, num);
-        }
+        // for(int num: nums){
+        //     maxNum = max(maxNum, num);
+        // }
+
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     if(nums[i] != 0 && nums[i] != maxNum){
+        //         double res = maxNum / nums[i];
+        //         if(res < 2){
+        //             return -1;
+        //         }
+        //     }
+
+        //     if(nums[i] == maxNum){
+        //         maxNumIndex = i;
+        //     }
+        // }
+
+        // return maxNumIndex;
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        int maxNum = -1;
+        int index = 0;
 
         for(int i = 0 ; i < nums.size() ; i++){
-            if(nums[i] != 0 && nums[i] != maxNum){
-                double res = maxNum / nums[i];
-                cout << res << endl;
+            if(nums[i] > maxNum){
+                maxNum = nums[i];
+                index = i;
+            }
+        }
+
+        for(int num: nums){
+            if(num != 0 && num != maxNum){
+                double res = maxNum / num;
                 if(res < 2){
                     return -1;
                 }
             }
-
-            if(nums[i] == maxNum){
-                maxNumIndex = i;
-            }
         }
-
-        return maxNumIndex;
+        return index;
     }
 };
