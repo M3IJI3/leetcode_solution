7aeec440c341dc1177a0ce96eeaa86207f717da4
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-13 12:21:04 -0700

    Time: 0 ms (100%), Space: 11.5 MB (82.02%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -7,21 +7,25 @@ public:
         vector<int> path;
         backtrack(nums, result, path, visited);
 
-        set<vector<int>> uniqueSet(result.begin(), result.end());
-        vector<vector<int>> ans(uniqueSet.begin(), uniqueSet.end());
+        // set<vector<int>> uniqueSet(result.begin(), result.end());
+        // vector<vector<int>> ans(uniqueSet.begin(), uniqueSet.end());
+        // return ans;
 
-        return ans;
+        return result;
     }
 private:
-    void backtrack(const vector<int>& nums, vector<vector<int>>& result, vector<int>& path, vector<bool>& visited){
+    void backtrack(vector<int>& nums, vector<vector<int>>& result, vector<int>& path, vector<bool>& visited){
         if(path.size() == nums.size()){
             result.push_back(path);
             return;
         }
 
+        // 先排序 这样重复的元素会相邻
+        sort(nums.begin(), nums.end());
         for(int i = 0 ; i < nums.size() ; i++){
             if(visited[i]) continue;
-            
+            if(i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+
             path.push_back(nums[i]);
             visited[i] = true;
 
