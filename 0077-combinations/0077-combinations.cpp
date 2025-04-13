e4e59061cc857c93f577e89ad41cfac022cf7d84
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-13 13:05:42 -0700

    Time: 35 ms (90.43%), Space: 62.4 MB (80.47%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,33 +1,59 @@
-class Solution {
+// class Solution {
+// public:
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<vector<int>> result;
+
+//         vector<int> path;
+//         vector<bool> visited(n , false);
+
+//         vector<int> nums;
+//         for(int i = 1 ; i <= n ; i++){
+//             nums.push_back(i);
+//         }
+
+//         backtrack(result, visited, nums, path, k);
+//         return result;
+//     }
+// private:
+//     void backtrack(vector<vector<int>>& result, vector<bool>& visited, const vector<int>& nums, vector<int>& path, int k){
+//         if(path.size() == k){
+//             result.push_back(path);
+//             return;
+//         }
+
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             if(visited[i]) return;
+
+//             path.push_back(nums[i]);
+//             visited[i] = true;
+//             backtrack(result, visited, nums, path, k);
+//             visited[i] = false;
+//             path.pop_back();
+//         }
+//     }
+// };
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+class Solution{
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> result;
-
         vector<int> path;
-        vector<bool> visited(n , false);
-
-        vector<int> nums;
-        for(int i = 1 ; i <= n ; i++){
-            nums.push_back(i);
-        }
-
-        backtrack(result, visited, nums, path, k);
+        backtrack(n, k, 1, path, result);
         return result;
     }
+
 private:
-    void backtrack(vector<vector<int>>& result, vector<bool>& visited, const vector<int>& nums, vector<int>& path, int k){
+    void backtrack(int n, int k, int start, vector<int>& path, vector<vector<int>>& result){
         if(path.size() == k){
             result.push_back(path);
             return;
         }
 
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(visited[i]) return;
-
-            path.push_back(nums[i]);
-            visited[i] = true;
-            backtrack(result, visited, nums, path, k);
-            visited[i] = false;
+        for(int i = start ; i <= n ; i++){
+            path.push_back(i);
+            backtrack(n, k, i + 1, path, result);
             path.pop_back();
         }
     }
