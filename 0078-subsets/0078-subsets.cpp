c9c41b78ec51157444fa809a856f701af853ecef
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-13 23:32:02 -0700

    Time: 0 ms (100%), Space: 9.8 MB (81.7%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-
         vector<int> path;
         backtrack(res, path, nums, 0);
         return res;
@@ -12,7 +11,6 @@ private:
     void backtrack(vector<vector<int>>& res, vector<int>& path, vector<int>& nums, int start){
         // 每一步都加入子集
         res.push_back(path);
-
         for(int i = start ; i < nums.size() ; i++){
             path.push_back(nums[i]);
             backtrack(res, path, nums, i + 1);
