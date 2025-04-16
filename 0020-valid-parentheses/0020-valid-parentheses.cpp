3c459f5895ab6780b37904b5e15024077b13c189
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-16 15:30:44 -0700

    Time: 0 ms (100%), Space: 9 MB (15.78%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> map = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        stack<char> stk;
+
+        for(char c : s){
+            if(!stk.empty() && map[c] == stk.top()){
+                stk.pop();
+                continue;
+            }
+            stk.push(c);
+        }
+        return stk.empty();
+    }
+};
