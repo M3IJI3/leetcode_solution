84e56b507f2e4abf9a26ca88d4b05bd0a354daf1
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-17 17:58:40 -0700

    Time: 0 ms (100%), Space: 10.1 MB (67.66%) - LeetHub

--- 1417-reformat-the-string/1417-reformat-the-string.cpp
+++ 1417-reformat-the-string/1417-reformat-the-string.cpp
@@ -5,25 +5,44 @@ public:
         string digits;
 
         for(char c : s){
-            if(c < 'a') digits += c;
+            if(isdigit(c)) digits += c;
             else letters += c; 
         }
 
-        int diff = letters.size() - digits.size();
+        if(abs((int)letters.size() - (int)digits.size()) > 1) return "";
 
-        if(abs(diff) > 1) return "";
+        if(letters.size() < digits.size()) swap(letters, digits);
 
         int i = 0, j = 0;
         string ans;
         while(i < letters.size() || j < digits.size()){
-            if(letters.size() > digits.size()){
-                if(i < letters.size()) ans += letters[i++];
-                if(j < digits.size()) ans += digits[j++];
-            } else {
-                if(j < digits.size()) ans += digits[j++];
-                if(i < letters.size()) ans += letters[i++];
-            }
+            if(i < letters.size()) ans += letters[i++];
+            if(j < digits.size()) ans += digits[j++];
         }
         return ans;
     }
 };
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// class Solution {
+// public:
+//     string reformat(string s) {
+//         string letters, digits;
+//         for (char c : s) {
+//             if (isalpha(c)) letters += c;
+//             else digits += c;
+//         }
+
+//         if (abs((int)letters.size() - (int)digits.size()) > 1) return "";
+
+//         // 让 letters 始终是更多的一方
+//         if (digits.size() > letters.size()) swap(letters, digits);
+
+//         string res;
+//         for (int i = 0; i < s.size(); ++i) {
+//             if (i % 2 == 0) res += letters[i / 2];
+//             else res += digits[i / 2];
+//         }
+//         return res;
+//     }
+// };
