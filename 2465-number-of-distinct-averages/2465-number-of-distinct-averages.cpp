9b99569c641a596e1f428997de1d88c7f7ba8de7
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-18 13:59:19 -0700

    Time: 0 ms (100%), Space: 9.8 MB (18.71%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -7,7 +7,6 @@ public:
         int left = 0, right = nums.size() - 1;
         while(left < right){
             double avg = (nums[left] + nums[right]) / 2.0;
-            cout << avg << endl;
             set.insert(avg);
 
             left++;
