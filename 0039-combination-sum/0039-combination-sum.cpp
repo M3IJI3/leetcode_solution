280f35e1cf9cef94114d110d6fa7c5414365a71b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-18 13:15:41 -0700

    Time: 3 ms (56.4%), Space: 14 MB (73.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,15 +1,43 @@
+/////////////
+// 1st try //
+/////////////
+// class Solution {
+// public:
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         sort(candidates.begin(), candidates.end());
+//         vector<vector<int>> result;
+//         vector<int> path;
+//         backtrack(result, path, candidates, target, 0);
+//         return result;
+//     }
+
+// private:
+//     void backtrack(vector<vector<int>>& result, vector<int>& path, const vector<int>& candidates, int target, int start){
+//         if(target == 0){
+//             result.push_back(path);
+//             return;
+//         }
+
+//         for(int i = start ; i < candidates.size() ; i++){
+//             if(candidates[i] > target) break;
+//             path.push_back(candidates[i]);
+//             backtrack(result, path, candidates, target - candidates[i], i);
+//             path.pop_back();
+//         }
+//     }
+// };
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> result;
         vector<int> path;
-        backtrack(result, path, candidates, target, 0);
+        backtrack(candidates, result, path, target, 0);
         return result;
     }
-
 private:
-    void backtrack(vector<vector<int>>& result, vector<int>& path, const vector<int>& candidates, int target, int start){
+    void backtrack(const vector<int>& candidates, vector<vector<int>>& result, vector<int>& path, int target, int start){
         if(target == 0){
             result.push_back(path);
             return;
@@ -18,7 +46,7 @@ private:
         for(int i = start ; i < candidates.size() ; i++){
             if(candidates[i] > target) break;
             path.push_back(candidates[i]);
-            backtrack(result, path, candidates, target - candidates[i], i);
+            backtrack(candidates, result, path, target - candidates[i], i);
             path.pop_back();
         }
     }
