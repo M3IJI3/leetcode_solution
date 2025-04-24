3a9140df3b7117ff1230c25614bfaa1d5c6ad253
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-23 20:48:22 -0700

    Time: 0 ms (100%), Space: 20.5 MB (69.1%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -4,21 +4,18 @@ public:
         int left = 0, right = nums.size() - 1;
 
         while(left < right){
-            // 如果左指针是奇数, 准备被替换
+            // 如果左指针是奇数
             if(nums[left] % 2 != 0){
-                // 并且右指针是偶数
                 if(nums[right] % 2 == 0){
                     int temp = nums[left];
                     nums[left] = nums[right];
                     nums[right] = temp;
-                    
-                    left++;
+                } else {
                     right--;
-                    continue;
                 }
-                right--;
-            } 
-            left++;
+            } else {
+                left++;
+            }
         }
         return nums;
     }
