750235677b1fdf1a91074715be7e73f66e8b0bdf
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-24 18:41:18 -0700

    Time: 0 ms (100%), Space: 12 MB (8.13%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> ans;
+
+        int i = 0;
+        int numI = 0, numD = s.size();
+
+        while(i < s.size()){
+            if(s[i] == 'I' && numI != numD){
+                ans.push_back(numI);
+                numI++;
+            } else {
+                ans.push_back(numD);
+                numD--;
+            }
+            i++;
+        }
+
+        ans.push_back(numI);
+        return ans;
+    }
+};
