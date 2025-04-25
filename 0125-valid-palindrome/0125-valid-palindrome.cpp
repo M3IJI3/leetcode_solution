283b6092be2a508f179dd75856d55ee986933c16
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-04-25 15:56:59 -0700

    Time: 4 ms (17.62%), Space: 9.8 MB (94.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp;
-        for(char c : s){
-            if(isalnum(c)) temp += tolower(c);
-        }
+        // string temp;
+        // for(char c : s){
+        //     // isalnum() 自动判断是字母还是数字
+        //     if(isalnum(c)) temp += tolower(c);
+        // }
         
-        int head = 0, tail = temp.size() - 1;
+        // int head = 0, tail = temp.size() - 1;
 
-        while(head < tail){
-            if(temp[head] != temp[tail]) return false;
-            head++;
-            tail--;
+        // while(head < tail){
+        //     if(temp[head] != temp[tail]) return false;
+        //     head++;
+        //     tail--;
+        // }
+
+        // return true;
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        // 在原字符串上操作
+        int left = 0, right = s.size() - 1;
+
+        while(left < right){
+            while(left < right && !isalnum(s[left])) left++;
+            while(left < right && !isalnum(s[right])) right--;
+
+            if(tolower(s[left]) != tolower(s[right])) return false;
+            left++;
+            right--;
         }
 
         return true;
