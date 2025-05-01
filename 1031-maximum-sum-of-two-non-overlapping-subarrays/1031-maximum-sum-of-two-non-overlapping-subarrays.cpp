bd0b841275bd6bc89df437d2ef80a50886dea132
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-01 00:08:11 -0700

    Time: 0 ms (100%), Space: 12.4 MB (54.21%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        return max(maxSum(nums, firstLen, secondLen), maxSum(nums, secondLen, firstLen)); 
+    }
+private:
+    int maxSum(vector<int>& nums, int L, int M){
+        int n = nums.size();
+        vector<int> prefix(n + 1, 0);
+
+        // 计算前缀和
+        for(int i = 0 ; i < n ; i++){
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        int maxL = 0; // 到当前位置之前L长度子数组的最大和
+        int result = 0;
+
+        for(int i = L + M ; i <= n ; i++){
+            maxL = max(maxL, prefix[i - M] - prefix[i - M - L]);
+            result = max(result, maxL + prefix[i] - prefix[i - M]);
+        }
+        return result;
+    }
+};
