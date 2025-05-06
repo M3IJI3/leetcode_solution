764761a606cc98358b4cd2b306e158899b114e75
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-05 22:44:29 -0700

    Time: 0 ms (100%), Space: 14.9 MB (21.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // int n = nums.size();
+        unordered_map<int, int> map;
 
-        // for(int i = 0 ; i < n - 1 ; i++){
-        //     for(int j = i + 1 ; j < n ; j++){
-        //         if(nums[i] + nums[j] == target){
-        //             return {i , j};
-        //         }
-        //     }
-        // }
-        // return {};
-        /////////////////////////////////////////////
-
-        // unordered_map<int, int> numMap; // define hashtable
-        // int n = nums.size();            // length of array
-
-        // for(int i = 0 ; i < n ; i++)
-        // {
-        //     numMap[nums[i]] = i;
-        // }
-
-        // for(int i = 0 ; i < n ; i++)
-        // {
-        //     int complement = target - nums[i];
-        //     if(numMap.count(complement) && nums[complement] != i){
-        //         return {i, numMap[complement]};
-        //     }
-        // }
-        // return {};
-        //////////////////////////////////////////////
-
-        unordered_map<int, int> numMap;
-        int n = nums.size();
-
-        for(int i = 0 ; i < n ; i++)
-        {
+        for(int i = 0 ; i < nums.size() ; i++){
             int complement = target - nums[i];
-            if(numMap.count(complement)){
-                return {numMap[complement], i};
+
+            if(map.count(complement)){
+                return { map[complement], i };
             }
-            numMap[nums[i]] = i;
+
+            map[nums[i]] = i;
         }
+
         return {};
     }
 };
