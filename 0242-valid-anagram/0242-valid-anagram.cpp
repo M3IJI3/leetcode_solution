cc41b35fa183b5c0597dc871ebf8a87d3b46d564
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-05 22:06:42 -0700

    Time: 1 ms (63.15%), Space: 9.7 MB (74.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> sfreq(26, 0);
-        vector<int> tfreq(26, 0);
+        vector<int> count(26, 0);
 
         if(s.size() != t.size()) return false;
-
+        
         for(int i = 0 ; i < s.size() ; i++){
-            char cs = s[i];
-            char ct = t[i];
-
-            sfreq[cs - 'a']++;
-            tfreq[ct - 'a']++;
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
 
         for(int i = 0 ; i < 26 ; i++){
-            if(sfreq[i] != tfreq[i]) return false;
+            if(count[i] != 0) return false;
         }
-
         return true;
     }
 };
