fbebe8c10a4bc8467e576ba4d7270c6b98b2bede
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-08 08:15:25 -0700

    Time: 0 ms (100%), Space: 11.6 MB (97.63%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,26 +3,11 @@ public:
     int removeElement(vector<int>& nums, int val) {
         // 快慢指针
         int slow = 0;
-        for(int fast = 0 ; fast < nums.size() ; fast++){
+        for(int fast = 0 ;  fast < nums.size() ; fast++){
             if(nums[fast] != val){
                 nums[slow++] = nums[fast];
             }
-            cout << slow << ", " << fast << endl;
         }
         return slow;
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-        // int left = 0 ;
-        // int right = nums.size() - 1;
-
-        // while(left <= right){
-        //     if(nums[left] == val){
-        //         nums[left] = nums[right];
-        //         right--;
-        //     } else {
-        //         left++;
-        //     }
-        // }
-        // return left;
     }
 };
