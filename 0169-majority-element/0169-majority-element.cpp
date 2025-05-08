51788eec681067232cf0cbefe23d503f4e7e678c
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-08 08:46:32 -0700

    Time: 0 ms (100%), Space: 28.1 MB (58.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,10 +4,10 @@ public:
         int ans, majority = 0;
 
         for (int num : nums) {
-            if (majority == 0) {
-                ans = num;
+            if (majority == 0) { // 表示当前没有候选人
+                ans = num;       // 更新新的候选人
             }
-            majority += ans == num ? 1 : -1;
+            majority += ans == num ? 1 : -1; 
         }
 
         return ans;
