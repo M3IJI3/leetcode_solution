10abf3b47473667768462f371885231df840142a
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-08 08:28:34 -0700

    Time: 0 ms (100%), Space: 22.4 MB (96.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int slow = 1;
         for(int fast = 1 ; fast < nums.size() ; fast++){
-            if(nums[fast] != nums[fast - 1]){
+            if(nums[fast] != nums[slow - 1]){
                 nums[slow++] = nums[fast];
             }
         }
