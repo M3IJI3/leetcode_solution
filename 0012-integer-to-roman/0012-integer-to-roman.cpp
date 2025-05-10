8a530de90f7035588ef17dc3044269c5a706b47b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-10 11:39:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        unordered_map<int, char> map = {
+            {1, 'I'},
+            {1, 'I'},
+            {1, 'I'},
+        }
+    }
+};
