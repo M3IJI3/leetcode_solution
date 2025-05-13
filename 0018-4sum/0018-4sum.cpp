aef236d447986a7bc6dfd15277a71476161c3d80
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-13 08:03:35 -0700

    Time: 15 ms (90.77%), Space: 17.4 MB (58.87%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> answer;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<vector<int>> ans;
 
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i - 1] == nums[i])
-                continue; // 去重
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j - 1] == nums[j])
-                    continue; // 去重
+        for(int i = 0 ; i < n - 3 ; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-                int left = j + 1, right = n - 1;
+            for(int j = i + 1 ; j < n - 2 ; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int x = j + 1;
+                int y = n - 1;
+
                 long long new_target = (long long)target - nums[i] - nums[j];
-                while (left < right) {
-                    int sum = nums[left] + nums[right];
+                while(x < y){
+                    long long sum = (long long)nums[x] + nums[y];
+                    if(sum == new_target){
+                        answer.push_back({nums[i], nums[j], nums[x], nums[y]});
+                        // 记住每次将结果计入时 需要立即更新索引位置
+                        x++;
+                        y--;
 
-                    if (sum == new_target) {
-                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while(x < y && nums[x] == nums[x-1]) x++;
+                        while(x < y && nums[y] == nums[y+1]) y--;
 
-                        while (left < right && nums[left + 1] == nums[left])
-                            left++;
-                        while (left < right && nums[right - 1] == nums[right])
-                            right--;
-
-                        left++;
-                        right--;
-                    } else if (sum < new_target) {
-                        left++;
+                    } else if(sum < new_target){
+                        x++;
                     } else {
-                        right--;
+                        y--;
                     }
                 }
             }
         }
-        return ans;
+
+        return answer;
     }
 };
