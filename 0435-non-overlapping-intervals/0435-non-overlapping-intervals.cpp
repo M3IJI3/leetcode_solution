0c68cedc3597fae89e5a1798d0bd86bdb744d397
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-14 13:37:48 -0700

    Time: 42 ms (73.83%), Space: 94 MB (66.7%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,15 +4,12 @@ public:
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1];
         });
-
-        for(auto& interval : intervals) cout << interval[0] << "," << interval[1] << " ";
         
         int count = 0;
         int lastInterval = intervals[0][1];
         for(int i = 1 ; i < intervals.size() ; i++){
             if(intervals[i][0] < lastInterval){
                 count++;
-                // lastInterval = intervals[i - 1][1];
             } else {
                 lastInterval = intervals[i][1];
             }
