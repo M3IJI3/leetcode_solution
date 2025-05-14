a7a0c1d6da86b87416bfd5aa8bcf8db869272cd2
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-13 19:58:40 -0700

    Time: 0 ms (100%), Space: 9.4 MB (9.89%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        // vector<string> ans;
-        // int left = 0;
-        // int right = 1;
+        vector<string> res;
+        int n = nums.size();
+        if(n == 0) return res;
 
-        // for(int i  = 0 ; i < nums.size() - 1 ; i++){
-        //     if(nums[right] - nums[i] == 1){
-        //         right += 1;
-        //     } else {
-        //         std::string str = std::to_string(nums[left]) + "->" + std::to_string(nums[right - 1]);
-        //         left = right;
-        //         ans.push_back(str);
-        //     }
-        // }
-        // return ans;
-        ////////////////////////////////////////////
+        int start = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(i + 1 < n && nums[i] + 1 == nums[i + 1]) continue;
 
-        vector<string> result;
-        if(nums.empty()) return result;
-
-        int start = nums[0];
-        for(int i = 0 ; i < nums.size() ; i++){
-            // 1. check if the index 'i' reach the last element
-            // 2. check if the next element is consecutive, by subtracting the adjacent elements
-            if(i == nums.size() - 1 || nums[i + 1] != 1 + nums[i]){
-                // Check if the start element is same with the current one
-                // if it does,
-                if(start == nums[i]){
-                    result.push_back(std::to_string(start));
-                } 
-                // if it doesn't,
-                else {
-                    result.push_back(std::to_string(start) + "->" + std::to_string(nums[i]));
-                }
-
-                // update 'start' if i haven't reach to last
-                if(i < nums.size() - 1){
-                    start = nums[i + 1];
-                }
+            if(start == i){
+                res.push_back(to_string(nums[start]));
+            } else {
+                res.push_back(to_string(nums[start]) + "->" + to_string(nums[i]));
             }
-        }
-        return result;
+            start = i + 1;
+            // cout << i << "," << start << endl;
+        } 
+        return res;
     }
 };
