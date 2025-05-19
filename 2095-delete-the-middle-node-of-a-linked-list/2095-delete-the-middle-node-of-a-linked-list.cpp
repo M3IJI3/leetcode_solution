17bb2d8dded741d0805f9f9a316fcb7289dc4fd4
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-19 15:01:23 -0700

    Time: 12 ms (9.22%), Space: 312.2 MB (18.06%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,35 +11,50 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        // 计算链表长度
-        int n = 0;
-        ListNode* start = head;
-        while(start){
-            start = start->next;
-            n++;
-        }
+        // // 计算链表长度
+        // int n = 0;
+        // ListNode* start = head;
+        // while(start){
+        //     start = start->next;
+        //     n++;
+        // }
 
-        if(n == 1) return nullptr;
+        // if(n == 1) return nullptr;
 
-        // 找到中点
+        // // 找到中点
+        // ListNode* slow = head;
+        // ListNode* fast = head;
+        // while(fast->next && fast->next->next){
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        // }
+
+        // // 判断两种情况
+        // ListNode* curr = head;
+        // if(n % 2 != 0){
+        //     for(int i = 1 ; i < n / 2 ; i++){
+        //         curr = curr->next;
+        //     }
+        //     curr->next = slow->next;
+        // } else {
+        //     slow->next = slow->next->next;
+        // }
+
+        // return head;
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        if(!head || !head->next) return nullptr;
+        
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast->next && fast->next->next){
+        ListNode* prev = nullptr;
+
+        while(fast && fast->next){
+            prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-
-        // 判断两种情况
-        ListNode* curr = head;
-        if(n % 2 != 0){
-            for(int i = 1 ; i < n / 2 ; i++){
-                curr = curr->next;
-            }
-            curr->next = slow->next;
-        } else {
-            slow->next = slow->next->next;
-        }
-
+        
+        prev->next = slow->next;
         return head;
     }
 };
