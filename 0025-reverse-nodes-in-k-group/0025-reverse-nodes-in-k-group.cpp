c73fca248b164e57377f3d8f7c3f55b753419c85
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-19 12:14:30 -0700

    Time: 0 ms (100%), Space: 16.4 MB (70.02%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -13,12 +13,11 @@ public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         int n = 1;
         ListNode* temp = head;
-        while(temp->next){
+        while (temp->next) {
             temp = temp->next;
             n++;
         }
 
-        // 虚拟头节点
         ListNode dummy(0);
         dummy.next = head;
 
@@ -26,27 +25,24 @@ public:
         ListNode* curr = head;
 
         int groupCount = n / k;
-        while(groupCount > 0){
+        while (groupCount > 0) {
             ListNode* groupHead = curr;
             ListNode* prev = nullptr;
 
-            for(int i = 0 ; i < k ; i++){
+            for (int i = 0; i < k; i++) {
                 ListNode* nextTemp = curr->next;
                 curr->next = prev;
                 prev = curr;
                 curr = nextTemp;
             }
 
-            // 接回前一组和后一组
             prevGroupEnd->next = prev;
             groupHead->next = curr;
 
-            // 更新 prevGroupEnd, 准备处理下一组
             prevGroupEnd = groupHead;
 
             groupCount--;
         }
-
         return dummy.next;
     }
 };
