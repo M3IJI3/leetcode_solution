75a56c2531444fc2b0aa50504d55331842fee07d
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-21 11:13:27 -0700

    Time: 8 ms (29.07%), Space: 15 MB (71.68%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,27 +17,6 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // if(!head) return nullptr;
-
-        // ListNode* curr = head;
-        // unordered_map<Node*, Node*> old2new;
-        // while(curr){
-        //     // 旧节点          新节点  
-        //     old2new[curr] = new ListNode(curr->val);
-        //     curr = curr->next;
-        // }
-
-        // curr = head;
-        // while(curr){
-        //     // A'->next = B'
-        //     old2new[curr]->next = old2new[curr->next];
-        //     old2new[curr]->random = old2new[curr->random];
-        //     curr = curr->next;
-        // }
-
-        // return old2new[head];
-
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         if(!head) return nullptr;
 
         Node* curr = head;
@@ -48,6 +27,7 @@ public:
             curr = copy->next;
         }
 
+        // 设置每个新节点的random指针
         curr = head;
         while(curr){
             if(curr->random){
@@ -56,22 +36,42 @@ public:
             curr = curr->next->next;
         }
 
-        Node dummy(0);
-        Node* copyCurr = &dummy;
-
+        // 断开链子
+        Node* dummy = new Node(0);
+        Node* copyCurr = dummy;
         curr = head;
         while(curr){
             Node* copy = curr->next;
             copyCurr->next = copy;
-            copyCurr = copyCurr->next;
-            
+            copyCurr = copy;
+
             curr->next = copy->next;
             curr = curr->next;
         }
-        
-        return dummy.next;
+
+        return dummy->next;
     }
 };
+
+// if(!head) return nullptr;
+
+// ListNode* curr = head;
+// unordered_map<Node*, Node*> old2new;
+// while(curr){
+//     // 旧节点          新节点  
+//     old2new[curr] = new Node(curr->val);
+//     curr = curr->next;
+// }
+
+// curr = head;
+// while(curr){
+//     // A'->next = B'
+//     old2new[curr]->next = old2new[curr->next];
+//     old2new[curr]->random = old2new[curr->random];
+//     curr = curr->next;
+// }
+
+// return old2new[head];
 
 
 
