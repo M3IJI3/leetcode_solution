3e83c39f15f68593fd31e5e725e88711ffe3df61
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-21 11:49:35 -0700

    Time: 0 ms (100%), Space: 11.2 MB (72.14%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -11,24 +11,20 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(!head || left == right) return head;
-
         ListNode dummy(0);
         dummy.next = head;
         ListNode* prev = &dummy;
 
-        // 定位到left的前一个节点
         for(int i = 1 ; i < left ; i++){
             prev = prev->next;
         }
 
-        // 开始原地反转
-        ListNode* curr = prev->next; // 当前正在处理的节点
-        for(int i = 0 ; i < right - left; i++){
+        ListNode* curr = prev->next;
+        for(int i = 0 ; i < right - left ; i++){
             ListNode* next = curr->next;
-            curr->next = next->next; // 这一步把 2 指向 4
-            next->next = prev->next; // 3 指向 2
-            prev->next = next; //  最后 1 指向 3
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
         }
 
         return dummy.next;
