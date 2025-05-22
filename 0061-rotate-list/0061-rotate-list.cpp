923005f1462680bb18c3cd491265cad4ed8d3091
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-22 10:52:24 -0700

    Time: 0 ms (100%), Space: 16.5 MB (30.67%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,32 +11,30 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k == 0) return head;
-
+        if(!head) return nullptr;
+        
         // 1. 计算链表长度
-        ListNode* tail = head;
         int len = 1;
+        ListNode* tail = head;
         while(tail->next){
             tail = tail->next;
             len++;
         }
 
-        // 2. 缩小旋转次数
+        // 2. 优化旋转次数
         k = k % len;
         if(k == 0) return head;
 
-        // 3 成环
+        // 3. 成环
         tail->next = head;
 
-        // 4. 找新尾结点
-        ListNode* newTail = head;
-        for(int i = 0 ; i < len - k - 1 ; i++){
-            newTail = newTail->next;
+        // 4. 找新尾节点
+        for(int i = 0; i < len - k; i++){
+            tail = tail->next;
         }
 
-        // 5. 断开
-        ListNode* newHead = newTail->next;
-        newTail->next = nullptr;
+        ListNode* newHead = tail->next;
+        tail->next = nullptr;
 
         return newHead;
     }
