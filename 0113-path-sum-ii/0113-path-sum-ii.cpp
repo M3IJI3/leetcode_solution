351b65af924f9138258384c856b8134f0afb3b20
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-26 12:38:27 -0700

    Time: 3 ms (52.14%), Space: 21.1 MB (44.92%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -10,38 +10,26 @@
  * };
  */
 class Solution {
+    vector<vector<int>> ans;
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> result;
         vector<int> path;
-        dfs(root, result, path, targetSum, 0);
-        return result;
+        dfs(root, targetSum, path);
+        return ans;
     }
 private:
-    void dfs(TreeNode* node, vector<vector<int>>& result, vector<int>& path, int targetSum, int sum){
-        // if(sum == targetSum && !node){
-        //     result.push_back(path);
-        //     return;
-        // }
-
-        // sum += node->val;
-        // dfs(node->left, result, path, targetSum, sum);
-        // dfs(node->right, result, path, targetSum, sum);
-        // path.pop_back();
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+    void dfs(TreeNode* node, int remain, vector<int>& path){
         if(!node) return;
 
         path.push_back(node->val);
-        sum += node->val;
+        remain -= node->val;
 
-        // 如果是叶子节点并且路径和等于目标值
-        if(!node->left && !node->right && sum ==  targetSum){
-            result.push_back(path);
+        if(!node->left && !node->right && remain == 0){
+            ans.push_back(path);
         }
 
-        dfs(node->left, result, path, targetSum, sum);
-        dfs(node->right, result, path, targetSum, sum);
+        dfs(node->left, remain, path);
+        dfs(node->right, remain, path);
 
         path.pop_back();
     }
