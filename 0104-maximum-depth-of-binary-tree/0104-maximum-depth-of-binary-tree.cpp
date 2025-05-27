d4a0cbb5dc398713cce2e00cc24ab9da60387378
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-27 15:34:08 -0700

    Time: 0 ms (100%), Space: 19.2 MB (13.42%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -29,7 +29,6 @@ public:
                 TreeNode* node = q.front(); q.pop();
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
-                levelSize--;
             }
             depth++;
         }
