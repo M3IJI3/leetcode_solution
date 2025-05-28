30c98a5a0fb2db8f543d57b65193e1486959d9df
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-05-28 13:06:45 -0700

    Time: 4 ms (14.17%), Space: 17 MB (88.12%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,42 +12,23 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        // bfs
-        // if(!root) return {};
+        if(!root) return {};
 
-        // vector<vector<int>> ans;
-        // queue<TreeNode*> q;
-        // q.push(root);
-
-        // while(!q.empty()){
-        //     int levelSize = q.size();
-        //     vector<int> path;
-        //     while(levelSize--){
-        //         TreeNode* node = q.front(); q.pop();
-        //         path.push_back(node->val);
-
-        //         if(node->left) q.push(node->left);
-        //         if(node->right) q.push(node->right);
-        //     }
-        //     ans.push_back(path);
-        // }
-        // return ans;
-
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-        // dfs
+        queue<TreeNode*> q;
+        q.push(root);
         vector<vector<int>> ans;
-        dfs(root, 0, ans);
+
+        while(!q.empty()){
+            int levelSize = q.size();
+            vector<int> curr;
+            while(levelSize--){
+                TreeNode* node = q.front(); q.pop();
+                curr.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            ans.push_back(curr);
+        }
         return ans;
-    }
-private:
-    void dfs(TreeNode* node, int depth, vector<vector<int>>& ans){
-        if(!node) return;
-
-        if(depth == ans.size()) ans.push_back({});
-
-        ans[depth].push_back(node->val);
-        dfs(node->left, depth + 1, ans);
-        dfs(node->right, depth + 1, ans);
     }
 };
