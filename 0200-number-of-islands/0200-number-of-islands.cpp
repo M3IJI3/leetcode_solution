22dcc19704b1adb90c682aee0e44e6ab24fd9e19
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-06-02 13:03:16 -0700

    Time: 34 ms (22.49%), Space: 23.8 MB (26.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,24 @@
 class Solution {
+    vector<vector<int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
         int rows = grid.size();
+        if(rows == 0) return 0;
         int cols = grid[0].size();
-        if(!rows) return 0;
-
-        vector<vector<bool>> visited(rows, vector<bool> (cols, false));
-        for(int row = 0; row < rows ; row++){
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false)); 
+        int answer = 0;
+        for(int row = 0 ; row < rows ; row++){
             for(int col = 0 ; col < cols ; col++){
                 if(grid[row][col] == '1' && !visited[row][col]){
                     bfs(grid, row, col, visited);
-                    count++;  
+                    answer++;
                 }
             }
         }
-        return count;
+        return answer;
     }
 
+    // 对每个点进行bfs
     void bfs(vector<vector<char>>& grid, int row, int col, vector<vector<bool>>& visited){
         queue<pair<int, int>> q;
         q.push({row, col});
@@ -25,7 +26,6 @@ public:
         while(!q.empty()){
             pair<int, int> tmp = q.front();
             q.pop();
-            vector<vector<int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
             for(int i = 0 ; i < dir.size() ; i++){
                 int tmp_row = tmp.first + dir[i][0];
                 int tmp_col = tmp.second + dir[i][1];
