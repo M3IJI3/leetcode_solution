a858d35599b8724adba720ef38b23b394ee6d05a
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-06-03 12:28:57 -0700

    Time: 0 ms (100%), Space: 11.9 MB (52.84%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -2,35 +2,36 @@ class Solution {
     unordered_map<string, vector<pair<string, double>>> graph;
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        // 1. build graph model
+        // 1. build the graph
         for(int i = 0 ; i < equations.size() ; i++){
             string a = equations[i][0];
             string b = equations[i][1];
             double val = values[i];
+
             graph[a].push_back({b, val});
             graph[b].push_back({a, 1.0 / val});
         }
 
-        // 2. do dfs to every search 
-        vector<double> res;
-        for(auto& q : queries){
-            string start = q[0], end = q[1];
+        vector<double> results;
+        for(auto& query : queries){
+            string start = query[0];
+            string end = query[1];
             unordered_set<string> visited;
-            double result = dfs(start, end, visited);
-            res.push_back(result);
+            double res = dfs(start, end, visited);
+            results.push_back(res);
         }
-        return res;
+        return results;
     }
 
     double dfs(string curr, string target, unordered_set<string>& visited){
-        if(!graph.count(curr) || !graph.count(target)) return -1.0;
+        if(!graph.count(curr)) return -1.0;
         if(curr == target) return 1.0;
 
         visited.insert(curr);
-        for(auto& [next, val] : graph[curr]){
+        for(auto& [next, weight] : graph[curr]){
             if(visited.count(next)) continue;
             double sub = dfs(next, target, visited);
-            if(sub != -1.0) return val * sub;
+            if(sub != -1.0) return weight * sub;
         }
         return -1.0;
     }
