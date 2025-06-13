1ca20568fe9acae693e7de795d2bd11626b53f8e
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-06-13 14:37:18 -0700

    Time: 546 ms (35.81%), Space: 346.2 MB (27.01%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -11,26 +11,21 @@ public:
 
         sort(dictionary.begin(), dictionary.end()); // 排序保证先找最短的root
 
-        vector<pair<string, string>> mapper;
+        string ans;
         for(int w_idx = 0 ; w_idx < words.size() ; w_idx++){
             bool found = false;  // 检测有没有找到 root
             string word = words[w_idx];
             for(int d_idx = 0 ; d_idx < dictionary.size() ; d_idx++){
                 string root = dictionary[d_idx];
                 if(word.rfind(root, 0) == 0){
-                    mapper.push_back({word, root});
+                    ans += root + " ";
                     found = true;
                     break;
                 }
             }
-            if(!found) mapper.push_back({word, word});
+            if(!found) ans += word + " ";
         }
-
-        string ans;
-        for(int i = 0 ; i < mapper.size() - 1 ; i++){
-            ans += mapper[i].second + " ";
-        } 
-        ans += mapper[mapper.size() - 1].second;
+        ans.pop_back();
         return ans;
     }
 };
