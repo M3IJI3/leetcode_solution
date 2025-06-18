75a6f85dd592a3ec08ff57fca2ef7e3721be0c0b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-06-18 13:51:52 -0700

    Time: 20 ms (62.95%), Space: 51.3 MB (23.09%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,69 @@
+class Trie {
+private:
+    struct Node{
+        bool endOfWord;
+        array<Node*, 26> next;      // 标记是否有单词在此结束
+        Node() : endOfWord(false){  // 指向26个子节点(a-z)
+            next.fill(nullptr);
+        }
+    };
+
+    Node* root; // 根节点
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* p = root;
+        for(char c : word){
+            int idx = c - 'a';
+            if(!p->next[idx]){
+                p->next[idx] = new Node;
+            }
+            p = p->next[idx];
+        }
+        p->endOfWord = true;
+    }
+    
+    bool search(string word) {
+        Node* p = root;
+        for(char c : word){
+            int idx = c - 'a';
+            if(!p->next[idx]) return false;
+            p = p->next[idx];
+        }
+        return p->endOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* p = root;
+        for(char c : prefix){
+            int idx = c - 'a';
+            if(!p->next[idx]) return false;
+            p = p->next[idx];
+        }
+        return true;
+    }
+
+    ~Trie(){
+        clear(root);
+    }
+
+private:
+    void clear(Node* p){
+        if(!p) return;
+        for(auto child : p->next){
+            clear(child);
+        }
+        delete p;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
