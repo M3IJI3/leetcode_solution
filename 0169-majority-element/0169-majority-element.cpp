1b1904dea8dea6e167bf515a8ef9064350ff570a
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-07-10 14:26:48 -0700

    Time: 3 ms (29.02%), Space: 28.3 MB (26.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ans, majority = 0;
-
-        for (int num : nums) {
-            if (majority == 0) { // 表示当前没有候选人
-                ans = num;       // 更新新的候选人
-            }
-            majority += ans == num ? 1 : -1; 
-        }
-
-        return ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        return nums[n / 2];
     }
 };
