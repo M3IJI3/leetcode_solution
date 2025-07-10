b696dfefcc1a255ac34b1111dd65b454a126bad8
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-07-10 14:53:33 -0700

    Time: 0 ms (100%), Space: 31 MB (6.21%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // k = k % nums.size();
-
-        // reverse(nums.begin(), nums.end());
-        // reverse(nums.begin(), nums.begin() + k);
-        // reverse(nums.begin() + k, nums.end());
-
         int n = nums.size();
-        k %= n;
-        int count = 0; // 记录被移动的元素个数
+        k = k % nums.size();
+        if(k == 0) return;
 
-        for (int start = 0; count < n; ++start) {
-            int current = start;
-            int prev = nums[start];
-
-            do {
-                int next = (current + k) % n;
-                int temp = nums[next];
-                nums[next] = prev;
-                prev = temp;
-                current = next;
-                count++;
-            } while (start != current); // 回到起点表示这个环结束
-        }
+        vector<int> temp;
+        for(int i = n - k ; i < n ; i++) temp.push_back(nums[i]);
+        for(int i = 0 ; i < n - k ; i++) temp.push_back(nums[i]);
+        nums = temp;
     }
 };
