81dc7880c4ec29a808e731b572b3352d3794a45b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-07-10 13:47:51 -0700

    Time: 0 ms (100%), Space: 12.4 MB (38.9%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;     // nums1 有效部分的末尾
-        int j = n - 1;     // nums2 末尾
-        int k = m + n - 1; // nums1 最终末尾
+        int i = m - 1;
+        int k = m + n - 1;
+        int j = n - 1;
 
         while(i >= 0 && j >= 0){
             if(nums1[i] > nums2[j]){
