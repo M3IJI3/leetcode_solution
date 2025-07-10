65e49fda4a90374364d58c4a3cb8a8bdf4e3a2b0
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-07-10 14:21:46 -0700

    Time: 7 ms (73.78%), Space: 19.5 MB (79.53%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // 每个元素最多出现两次
-        if(nums.empty()) return 0;
-        if(nums.size() <= 2) return nums.size();
-
+        if(nums.size() < 2) return nums.size();
         int slow = 2;
         for(int fast = 2 ; fast < nums.size() ; fast++){
-            if(nums[fast] != nums[slow - 2]){
-                nums[slow] = nums[fast];
-                slow++;
-            }
+            if(nums[fast] != nums[slow - 2])
+                nums[slow++] = nums[fast];
         }
         return slow;
     } 
