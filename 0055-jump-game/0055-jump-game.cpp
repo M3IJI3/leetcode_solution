b5bf5a670dded5b69f5c76aea62bc50a39da536f
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2025-07-11 14:56:23 -0700

    Time: 0 ms (100%), Space: 52.2 MB (87.1%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // 记录当前最远可以跳跃的位置
-        int maxReach = 0;
-
-        // 遍历当前位置到最远位置的元素
-        for(int i = 0 ; i <= maxReach ; i++){ // -> 对于每一个索引 i 都要更新最远可以跳到的位置
-            // 判断 maxReach 是否抵达终点
-            if(maxReach >= nums.size() - 1) return true;
-            // 更新最远可以跳到的位置
-            maxReach = max(maxReach, i + nums[i]);
+        int target = nums.size() - 1;
+        for(int i = nums.size() - 2 ; i >= 0 ; i--){
+            if(i + nums[i] >= target)
+                target = i;
         }
-        return false;
+        return target == 0;
     }
 };
