9e467617a583c515fa071bb39ab671b1e95ff0e3
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-04 17:14:57 +0800

    Time: 3 ms (62.14%), Space: 12.2 MB (77.67%) - LeetHub

--- 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        // 统计所有可用字符频率
         int have[26]{};
-        for(char& c : letters) have[c - 'a']++;
+        for(char c : letters) have[c - 'a']++;
 
         int n = words.size(), ans = 0;
         for(int mask = 0 ; mask < (1 << n) ; mask++){
-            int temp[26];
+            int temp[26]{};
             copy(begin(have), end(have), temp);
 
             int currScore = 0;
