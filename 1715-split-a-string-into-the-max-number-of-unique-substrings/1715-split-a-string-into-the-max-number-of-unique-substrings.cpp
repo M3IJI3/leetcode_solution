c93fe2a774ff55c786ec75ec8bdb45b84e2ea4a0
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-05 14:40:15 +0800

    Time: 265 ms (35.52%), Space: 46.2 MB (32.79%) - LeetHub

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int maxLen = 0;
+    unordered_set<string> seen;
+public:
+    int maxUniqueSplit(string s) {
+        vector<string> path;
+        dfs(s, path, 0);
+        return maxLen;
+    }
+
+    void dfs(string& s, vector<string>& path, int index){
+        if(index == s.size()){
+            maxLen = max(maxLen, (int)path.size());
+            return;
+        }    
+
+        for(int i = index ; i < s.size() ; i++){
+            string sub = s.substr(index, i - index + 1);
+            if(seen.count(sub)) continue;
+
+            seen.insert(sub);
+            path.push_back(sub);
+            dfs(s, path, i + 1);
+            path.pop_back();
+            seen.erase(sub);
+        }
+    }
+};
