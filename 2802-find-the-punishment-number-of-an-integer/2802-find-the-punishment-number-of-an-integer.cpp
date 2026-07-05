82f27d1fdcb20cbb12cab880a23718d9f5f14f5b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-05 14:24:51 +0800

    Time: 269 ms (32.34%), Space: 10.9 MB (37.3%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int punishmentNumber(int n) {
-        int ans = 0 ;
+        int ans = 0;
         for(int i = 1 ; i <= n ; i++){
-            int target = i * i;
-            string s = to_string(target);
-            if(dfs(s, 0, 0, i)){
-                ans += target;
+            int square = i * i;
+            string s = to_string(square);
+            if (dfs(s, 0, 0, 0, i)){
+                ans += square;
             }
         }
         return ans;
     }
 
-    bool dfs(string& s, int index, int sum, int target){
-        if(index == s.size()){
+    bool dfs(string& s, int i, int start, int sum, int target){
+        if(i == s.size()){
             return sum == target;
         }
 
-        int num = 0;
-        for(int i = index ; i < s.size() ; i++){
-            num = num * 10 + (s[i] - '0');
-            if(sum + num > target) return false;
-            if(dfs(s, i + 1, sum + num, target)){
+        if(i < s.size() - 1){
+            if(dfs(s, i + 1, start, sum, target)){
+                return true;
+            }
+        }
+
+        string sub = s.substr(start, i - start + 1);
+        int num = stoi(sub);
+        if(sum + num <= target){
+            if(dfs(s, i + 1, i + 1, sum + num, target)){
                 return true;
             }
         }
