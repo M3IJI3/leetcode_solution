8c85d421449cb3f686867fbf2f28f80eb9969455
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-05 12:01:15 +0800

    Time: 603 ms (5.22%), Space: 181.2 MB (7.46%) - LeetHub

--- 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
+++ 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,32 +1,29 @@
 class Solution {
-    int ans = 0;
 public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        vector<int> diff(n, 0);
-        dfs(requests, diff, 0, 0);
+        int m = requests.size();
+        int ans = 0;
+        for(int mask = 0 ; mask < (1 << m) ; mask++){
+            vector<int> diff(n, 0);
+            bool valid = true;
+            int count = 0;
+            for(int i = 0 ; i < m ; i++){
+                if(mask & (1 << i)){
+                    count++;
+                    int from = requests[i][0], to = requests[i][1];
+                    diff[from]--;
+                    diff[to]++;
+                }
+            }
+
+            for(int d : diff){
+                if(d != 0){
+                    valid = false;
+                    break;
+                }
+            }
+            if(valid) ans = max(ans, count);
+        }
         return ans;
     }
-
-    void dfs(vector<vector<int>>& requests, vector<int>& diff, int index, int count){
-        if(index == requests.size()){
-            for(int d : diff){
-                if(d != 0) return;
-            }
-            ans = max(ans, count);
-            return;
-        }
-
-        dfs(requests, diff, index + 1, count);
-
-        int from = requests[index][0];
-        int   to = requests[index][1];
-        diff[from]--;
-        diff[to]++;
-        dfs(requests, diff, index + 1, count + 1);
-        diff[to]--;
-        diff[from]++;
-    }
 };
-
-// 每栋楼 from 的员工数目 == 该楼 to 的员工数数目
-// 组合不同的请求列表, 保证变化量为0
