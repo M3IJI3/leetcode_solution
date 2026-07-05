1f70a69a90d1810d6ecb053f044990495b41616f
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-05 12:25:04 +0800

    Time: 23 ms (97.76%), Space: 11.4 MB (42.54%) - LeetHub

--- 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
+++ 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,29 +1,29 @@
 class Solution {
+    int ans = 0;
 public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        int m = requests.size();
-        int ans = 0;
-        for(int mask = 0 ; mask < (1 << m) ; mask++){
-            vector<int> diff(n, 0);
-            bool valid = true;
-            int count = 0;
-            for(int i = 0 ; i < m ; i++){
-                if(mask & (1 << i)){
-                    count++;
-                    int from = requests[i][0], to = requests[i][1];
-                    diff[from]--;
-                    diff[to]++;
-                }
-            }
-
-            for(int d : diff){
-                if(d != 0){
-                    valid = false;
-                    break;
-                }
-            }
-            if(valid) ans = max(ans, count);
-        }
+        vector<int> diff(n, 0);
+        dfs(requests, diff, 0, 0);
         return ans;
+    }
+
+    void dfs(vector<vector<int>>& requests, vector<int>& diff, int count, int index){
+        if(index == requests.size()){
+            for(int d : diff){
+                if(d != 0) return;
+            }
+            ans = max(ans, count);
+            return;
+        }
+
+        dfs(requests, diff, count, index + 1);
+
+        int from = requests[index][0];
+        int to = requests[index][1];
+        diff[from]--;
+        diff[to]++;
+        dfs(requests, diff, count + 1, index + 1);
+        diff[from]++;
+        diff[to]--;
     }
 };
