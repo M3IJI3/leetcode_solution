1c25df1ea7677a45b43b3bb2f03d0dff62a89e05
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-06 10:39:31 +0800

    Time: 0 ms (100%), Space: 15 MB (28.65%) - LeetHub

--- 1222-remove-covered-intervals/1222-remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/1222-remove-covered-intervals.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
-            if(a[0] == b[0]) a[1] > b[1];
+            if(a[0] == b[0]) return a[1] > b[1];
             return a[0] < b[0];
         });
 
