e7d6f87bd488cbfb07a257e88058ca26355dc1a3
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-06 22:10:22 +0800

    Time: 33 ms (33.02%), Space: 22.6 MB (16.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,34 @@
 class Solution {
-    vector<vector<int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        if(rows == 0) return 0;
-        int cols = grid[0].size();
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false)); 
-        int answer = 0;
-        for(int row = 0 ; row < rows ; row++){
-            for(int col = 0 ; col < cols ; col++){
-                if(grid[row][col] == '1' && !visited[row][col]){
-                    bfs(grid, row, col, visited);
-                    answer++;
-                }
-            }
-        }
-        return answer;
-    }
+        int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+        int m = grid.size(), n = grid[0].size();
+        
+        int islands = 0;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == '1'){
+                    islands++;
 
-    // 对每个点进行bfs
-    void bfs(vector<vector<char>>& grid, int row, int col, vector<vector<bool>>& visited){
-        queue<pair<int, int>> q;
-        q.push({row, col});
-        visited[row][col] = true;
-        while(!q.empty()){
-            pair<int, int> tmp = q.front();
-            q.pop();
-            for(int i = 0 ; i < dir.size() ; i++){
-                int tmp_row = tmp.first + dir[i][0];
-                int tmp_col = tmp.second + dir[i][1];
-                if(tmp_row >= 0 && tmp_row < grid.size() && tmp_col >= 0 && tmp_col < grid[0].size() && 
-                    !visited[tmp_row][tmp_col] && grid[tmp_row][tmp_col] == '1'){
-                    q.push({tmp_row, tmp_col});
-                    visited[tmp_row][tmp_col] = true;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = '0';
+
+                    while(!q.empty()){
+                        auto [x, y] = q.front(); q.pop();
+
+                        for(auto& d : dirs){
+                            int nx = x + d[0], ny = y + d[1];
+                            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                            if(grid[nx][ny] == '1'){
+                                grid[nx][ny] = '0';
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
                 }
             }
         }
+        return islands;
     }
 };
