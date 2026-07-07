c2ec164038e42734578a4c816ece407512aa3442
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-07 22:08:45 +0800

    Time: 4 ms (75.56%), Space: 23.5 MB (29.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> result;
-        if(intervals.empty()) return result;
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
+            return a[0] < b[0];
+        });
 
-        sort(intervals.begin(), intervals.end());
-
+        vector<vector<int>> ans;
         vector<int> current = intervals[0];
-        for(int i = 1 ; i < intervals.size() ; i++){
-            // 有overlap
+        for(int i = 1 ; i < intervals.size(); i++){
             if(current[1] >= intervals[i][0]){
                 current[1] = max(current[1], intervals[i][1]);
-            // 无overlap
             } else {
-                result.push_back(current);
+                ans.push_back(current);
                 current = intervals[i];
             }
         }
-
-        result.push_back(current);
-        return result;
+        ans.push_back(current);
+        return ans;
     }
 };
+
+// [1, 3][2, 6][8, 10][15, 18]
