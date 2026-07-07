b5581d307345b98d9f0556ebbe0564b434db1fc0
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-07 21:50:15 +0800

    Time: 4 ms (24.9%), Space: 70 MB (99.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> presum(n + 1);
-        for(int i = 0 ; i < n ; i++){
-            presum[i + 1] = presum[i] + nums[i];
+        int presum = 0, maxAns = nums[0];
+        for(int x : nums){
+            presum = max(presum + x, x);
+            maxAns = max(maxAns, presum);
         }
-
-        // 找右边的最大值 - 左边的最小值
-        int minPresum = presum[0];
-        int maxSum = INT_MIN;
-        for(int i = 1 ; i < presum.size() ; i++){
-            int currSum = presum[i] - minPresum;
-            maxSum = max(maxSum, currSum);
-            minPresum = min(minPresum, presum[i]);
-        }
-        return maxSum;
+        return maxAns;
     }
 };
-
-// 0 -2 -1 -4 0 -1 1 2 -3 1 
