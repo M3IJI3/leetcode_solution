9a7681dc002bf3a56bfad0dd1aea59b3262d9c11
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-07 13:12:36 +0800

    Time: 0 ms (100%), Space: 18.1 MB (11.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -6,18 +6,20 @@ public:
         int originalColor = image[sr][sc];
         if(originalColor == color) return image;
 
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        image[sr][sc] = color;
+        vector<pair<int, int>> q(m * n);
+        int head = 0, tail = 0;
 
-        while(!q.empty()){
-            auto [x, y] = q.front(); q.pop();
-            for(auto& d: dirs){
+        q[tail++] = {sr, sc};
+        image[sr][sc] = color;
+        while(head < tail){
+            auto [x, y] = q[head++];
+
+            for(auto& d : dirs){
                 int nx = x + d[0], ny = y + d[1];
                 if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
                 if(image[nx][ny] == originalColor){
-                    q.push({nx, ny});
                     image[nx][ny] = color;
+                    q[tail++] = {nx, ny};
                 }
             }
         }
