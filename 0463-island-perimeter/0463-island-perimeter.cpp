3a22df39fafe5c2bdc3d9a4abfa442d843e08378
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-07 12:24:33 +0800

    Time: 15 ms (25.56%), Space: 103.5 MB (6.79%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int rows = grid.size(), cols = grid[0].size();
-        int perimeter = 0;
-
-        for(int i = 0 ; i < rows ; i++){
-            for(int j = 0 ; j < cols ; j++){
-                // if it's land
+        int m = grid.size(), n = grid[0].size();
+        int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+        int ans = 0;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
                 if(grid[i][j] == 1){
-                    perimeter += 4;
+                    int perimeter = 0;
 
-                    // if there is land topside
-                    if(i > 0 && grid[i - 1][j] == 1){
-                        perimeter -= 2; // both lands share same edges
-                    }
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = -1;
 
-                    // if there is land leftside
-                    if(j > 0 && grid[i][j - 1] == 1){
-                        perimeter -= 2;
+                    while(!q.empty()){
+                        auto [x, y] = q.front(); q.pop();
+                        for(auto& d : dirs){
+                            int nx = x + d[0], ny = y + d[1];
+                            if(nx < 0 || nx >= m || ny < 0 || ny >= n){
+                                perimeter++;
+                                continue;
+                            }
+
+                            if(grid[nx][ny] == 0){
+                                perimeter++;
+                                continue;
+                            }
+
+                            if(grid[nx][ny] == -1) continue;
+
+                            if(grid[nx][ny] == 1){
+                                q.push({nx, ny});
+                                grid[nx][ny] = -1;
+                            } 
+                        }
                     }
+                    ans += perimeter;
                 }
             }
         }
-        return perimeter;
+        return ans;
     }
 };
