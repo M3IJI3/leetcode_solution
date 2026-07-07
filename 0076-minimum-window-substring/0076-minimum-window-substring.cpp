5a4bf912933cb31e82f199711ceb6350e40962cf
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-07 21:34:47 +0800

    Time: 15 ms (65.46%), Space: 11.3 MB (61.8%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,96 +1,44 @@
-// class Solution {
-// public:
-//     string minWindow(string s, string t) {
-//         if(s.empty() || t.empty()) return "";
-
-//         unordered_map<char, int> need, window;
-//         for(char c : t) need[c]++;
-
-//         int left = 0, right = 0;
-//         int valid = 0; // 满足的字符种类
-//         int start = 0, len = INT_MAX;
-
-//         while(left < s.size()){
-//             char c = s[right];
-//             right++;
-
-//             // 如果是需要的字符, 加入window中
-//             if(need.count(c)){
-//                 window[c]++;
-//                 if(window[c] == need[c]) valid++;
-//             }
-
-//             // 尝试收缩窗口
-//             while(valid == need.size()){
-//                 // 更新最小窗口
-//                 if(right - left < len){
-//                     start = left;
-//                     len = right - left;
-//                 }
-
-//                 char d = s[left];
-//                 left++;
-
-//                 if(need.count(d)){
-//                     if(window[d] == need[d]) valid--;
-//                     window[d]--;
-//                 }
-//             }
-//         }
-
-//         return len == INT_MAX ? "" : s.substr(start, len);
-//     }
-// };
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.empty() || t.empty()) return "";
+        int m = s.size(), n = t.size();
+        if(m < n) return "";
 
-        unordered_map<char, int> need, window;
-        for(char& c : t) need[c]++;  // 统计目标字符串字母出现的频率
+        unordered_map<char, int> need;
+        for(char c : t) need[c]++;
 
-        int left = 0;
+        unordered_map<char, int> window;
+        int left = 0 , right = 0;
         int valid = 0;
-        int start = 0, len = INT_MAX;
-        for(int right = 0 ; right < s.size() ; right++){
-            char letter = s[right];
+        int minLen = INT_MAX, start = 0;
+        while(right < m){
+            char c = s[right];
+            right++;
 
-            if(need.count(letter)){
-                window[letter]++;
-                if(window[letter] == need[letter]) valid++;
+            if(need.count(c)){
+                window[c]++;
+                if(window[c] == need[c]){
+                    valid++;
+                }
             }
 
-            // 开始收缩窗口
             while(valid == need.size()){
-                // 更新最小窗口
-                if(right - left + 1 < len){
+                if(right - left < minLen){
                     start = left;
-                    len = right - left + 1;
+                    minLen = right - left;
                 }
 
                 char d = s[left];
                 left++;
 
                 if(need.count(d)){
-                    if(window[d] == need[d]) valid--;
+                    if(window[d] == need[d]){
+                        valid--;
+                    }
                     window[d]--;
                 }
             }
         }
-        return len == INT_MAX ? "" : s.substr(start, len);
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
