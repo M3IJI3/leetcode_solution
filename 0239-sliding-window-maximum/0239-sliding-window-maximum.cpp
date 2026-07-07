34613826534b6e9f10177c15954cf3310f2983b3
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-07 21:00:18 +0800

    Time: 31 ms (56.96%), Space: 131.4 MB (92.89%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> window;
-        vector<int> ans;
-        
-
-        for(int i = 0 ; i < nums.size() ; i++){
-            window.push_back(nums[i]);
-
-            if(window.size() == k){
-                int maxWindow = INT_MIN;
-                for(int num : window){
-                    maxWindow = max(maxWindow, num);
-                }
-                ans.push_back(maxWindow);
-
-                window.pop_front();
+        int n =  nums.size();
+        vector<int> ans(n - k + 1);
+        deque<int> d;
+        for(int r = 0 ; r < nums.size() ; r++){
+            while(!d.empty() && nums[d.back()] <= nums[r]){
+                d.pop_back();
             }
+
+            d.push_back(r);
+
+            int l = r - k + 1;
+            if(l < 0) continue;
+
+            if(d.front() < l){
+                d.pop_front();
+            }
+            ans[l] = nums[d.front()];
         }
         return ans;
     }
