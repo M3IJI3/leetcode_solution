c20089b80e711edd2e1687eece70e9526195dd1b
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-08 12:10:10 +0800

    Time: 4 ms (30.02%), Space: 17 MB (9.28%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
@@ -2,36 +2,28 @@ class Solution {
 public:
     int closedIsland(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        int dirs[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
         int ans = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    // BFS 检查这个岛屿是否封闭
-                    queue<pair<int,int>> q;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 0){
+                    queue<pair<int, int>> q;
                     q.push({i, j});
-                    grid[i][j] = 2;  // 标记已访问
-                    bool closed = true;
+                    grid[i][j] = 2;
+                    bool isClosed = true;
 
-                    while (!q.empty()) {
+                    while(!q.empty()){
                         auto [x, y] = q.front(); q.pop();
-
-                        // ★ 如果这个点在边界上，说明不是封闭岛屿
-                        if (x == 0 || x == m - 1 || y == 0 || y == n - 1) {
-                            closed = false;
-                        }
-
-                        for (auto& d : dirs) {
+                        if(x == 0 || x == m - 1 || y == 0 || y == n - 1) isClosed = false;
+                        for(auto& d : dirs){
                             int nx = x + d[0], ny = y + d[1];
-                            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                            if (grid[nx][ny] != 0) continue;
+                            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                            if(grid[nx][ny] != 0) continue;
                             grid[nx][ny] = 2;
                             q.push({nx, ny});
                         }
                     }
-
-                    if (closed) ans++;
+                    if(isClosed) ans++;
                 }
             }
         }
