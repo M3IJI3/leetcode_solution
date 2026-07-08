5b95a22d71ce8906b1186aed007674f607d1cd23
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-08 15:48:05 +0800

    Time: 16 ms (32.52%), Space: 37.8 MB (33.51%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    const int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int m = maze.size(), n = maze[0].size();
-        int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
         int sx = entrance[0], sy = entrance[1];
-
         queue<pair<int, int>> q;
-        vector<vector<int>> dist(m, vector<int>(n, -1));
+        vector<vector<int>> dist(m , vector<int>(n, -1));
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(i == sx && j == sy) continue;
                 if(i == 0 || i == m - 1 || j == 0 || j == n - 1){
-                    if(maze[i][j] == '.'){
+                    if (maze[i][j] == '.'){
                         q.push({i, j});
                         dist[i][j] = 0;
                     }
@@ -24,7 +23,7 @@ public:
         while(!q.empty()){
             auto [x, y] = q.front(); q.pop();
             if(x == sx && y == sy) return dist[x][y];
-            for(auto& d : dirs){
+            for(auto& d: dirs){
                 int nx = x + d[0], ny = y + d[1];
                 if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
                 if(maze[nx][ny] == '+') continue;
