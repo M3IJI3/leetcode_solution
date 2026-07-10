937f41308786528ffb500763f6d7d8c815a31c05
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-10 12:34:02 +0800

    Time: 0 ms (100%), Space: 19.1 MB (52.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,25 +2,43 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n, false);
-        queue<int> q;
-        int provinces = 0;
+        vector<int> parent(n);
+
+        // 初始化
         for(int i = 0 ; i < n ; i++){
-            if(!visited[i]){
-                provinces++;
-                q.push(i);
-                visited[i] = true;
-                while(!q.empty()){
-                    int cur = q.front(); q.pop();
-                    for(int j = 0 ; j < n ; j++){
-                        if(isConnected[cur][j] == 1 && !visited[j]){
-                            visited[j] = true;
-                            q.push(j);
-                        }
-                    }
+            parent[i] = i;
+        }
+
+        // 合并
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                if(isConnected[i][j] == 1){
+                    unite(parent, i, j);
                 }
             }
         }
-        return provinces;
+
+        int province = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(find(parent, i) == i){
+                province++;
+            }
+        }
+        return province;
+    }
+
+    void unite(vector<int>& parent, int x, int y){
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+        if(rootX != rootY){
+            parent[rootY] = rootX;
+        }
+    }
+    
+    int find(vector<int>& parent, int x){
+        if(parent[x] != x){
+            parent[x] = find(parent, parent[x]);
+        }
+        return parent[x];
     }
 };
