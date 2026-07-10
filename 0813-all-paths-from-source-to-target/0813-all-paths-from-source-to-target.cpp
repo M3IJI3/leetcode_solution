1c0afbfcdfd7af09933e37094b2162d9d78ccca0
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-10 13:54:39 +0800

    Time: 35 ms (5.85%), Space: 23.8 MB (5.06%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    vector<vector<int>> ans;
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<int> path;
-        path.push_back(0);
-        dfs(graph, 0, graph.size() - 1, path);
+        int target = graph.size() - 1;
+        vector<vector<int>> ans;
+        queue<pair<int, vector<int>>> q;
+        q.push({0, {0}});
+        while(!q.empty()){
+            auto [curr, path] = q.front(); q.pop();
+            if(curr == target){
+                ans.push_back(path);
+                continue;
+            }
+
+            for(int next: graph[curr]){
+                vector<int> newPath = path;
+                newPath.push_back(next);
+                q.push({next, newPath});
+            }
+        }
         return ans;
-    }
-
-    void dfs(vector<vector<int>>& graph, int curr, int target, vector<int>& path){
-        if(curr == target){
-            ans.push_back(path);
-            return;
-        }
-
-        for(int next: graph[curr]){
-            path.push_back(next);
-            dfs(graph, next, target, path);
-            path.pop_back();
-        }
     }
 };
