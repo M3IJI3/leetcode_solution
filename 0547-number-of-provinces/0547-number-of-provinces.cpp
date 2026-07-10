dc9fe1a7c4e256d0df47cbb69404a7776bf342f2
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-10 12:47:06 +0800

    Time: 0 ms (100%), Space: 19.2 MB (27.67%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -3,15 +3,12 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         vector<int> parent(n);
-
-        // 初始化
         for(int i = 0 ; i < n ; i++){
             parent[i] = i;
-        }
+        }    
 
-        // 合并
         for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
+            for(int j = i ; j < n ; j++){
                 if(isConnected[i][j] == 1){
                     unite(parent, i, j);
                 }
@@ -27,18 +24,18 @@ public:
         return province;
     }
 
-    void unite(vector<int>& parent, int x, int y){
-        int rootX = find(parent, x);
-        int rootY = find(parent, y);
-        if(rootX != rootY){
-            parent[rootY] = rootX;
-        }
-    }
-    
     int find(vector<int>& parent, int x){
         if(parent[x] != x){
             parent[x] = find(parent, parent[x]);
         }
         return parent[x];
+    }
+
+    void unite(vector<int>& parent, int x, int y){
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+        if(rootX != rootY){
+            parent[rootX] = rootY;
+        }
     }
 };
