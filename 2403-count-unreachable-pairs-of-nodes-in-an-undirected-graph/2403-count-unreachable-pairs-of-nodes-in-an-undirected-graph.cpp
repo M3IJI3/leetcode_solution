7a78b8d22e20bbc9cd0d30d6729b6359a4ff0f6a
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-10 19:38:52 +0800

    Time: 237 ms (13.55%), Space: 279.6 MB (5.16%) - LeetHub

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     long long countPairs(int n, vector<vector<int>>& edges) {
-        // 建立邻接表
         vector<vector<int>> graph(n);
         for(auto& e : edges){
             int u = e[0], v = e[1];
@@ -14,22 +13,24 @@ public:
         long long total = n;
         for(int i = 0 ; i < n ; i++){
             if(!visited[i]){
-                long long size = dfs(graph, visited, i);
+                queue<int> q;
+                q.push(i);
+                visited[i] = true;
+                long long size = 0;
+                while(!q.empty()){
+                    auto curr = q.front(); q.pop();
+                    size++;
+                    for(auto& next : graph[curr]){
+                        if(!visited[next]){
+                            visited[next] = true;
+                            q.push(next);
+                        }
+                    }
+                }
                 total -= size;
-                ans += size * total;
+                ans += total * size;
             }
         }
         return ans;
-    }
-
-    int dfs(vector<vector<int>>& graph, vector<bool>& visited, int node){
-        visited[node] = true;
-        int size = 1;
-        for(auto& next: graph[node]){
-            if(!visited[next]){
-                size += dfs(graph, visited, next);
-            }
-        }
-        return size;
     }
 };
