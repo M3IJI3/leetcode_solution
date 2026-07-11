352ad96e3455b4105971c6f30161860bc09d70e3
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-11 14:32:26 +0800

    Time: 16 ms (87.48%), Space: 141.1 MB (8.34%) - LeetHub

--- 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,32 +1,35 @@
 class Solution {
+    vector<int> parent;
+
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y){
+        int rootx = find(x);
+        int rooty = find(y);
+        if(rootx != rooty){
+            parent[rooty] = rootx;
+        }
+    }
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> graph(n + 1);
-        for(auto& r : roads){
-            int u = r[0], v = r[1], d = r[1];
-            graph[u].push_back({v, d});
-            graph[v].push_back({u, d});
-        }
+        parent.resize(n + 1);
 
-        vector<bool> visited(n + 1, false);
-        dfs(graph, visited, 1);
+        for(int i = 0 ; i < n ; i++) parent[i] = i;
+        for(auto& r : roads) unite(r[0], r[1]);
 
+        int root = find(1);
         int ans = INT_MAX;
         for(auto& r : roads){
             int u = r[0], v = r[1], d = r[2];
-            if(visited[u] && visited[v]){
+            if(find(u) == root){
                 ans = min(ans, d);
             }
         }
         return ans;
-    }
-
-    void dfs(vector<vector<pair<int, int>>>& graph, vector<bool>& visited, int node){
-        visited[node] = true;
-        for(auto& [next, dist] : graph[node]){
-            if(!visited[next]){
-                dfs(graph, visited, next);
-            }
-        }
     }
 };
