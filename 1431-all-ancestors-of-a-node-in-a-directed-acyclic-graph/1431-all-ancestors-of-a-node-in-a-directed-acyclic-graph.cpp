ae497ee6b1b0566e5afbb599c44802849047c1fa
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-11 19:11:00 +0800

    Time: 75 ms (64.13%), Space: 71.6 MB (33.69%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -3,34 +3,31 @@ public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         // 1. 建反向图
         vector<vector<int>> graph(n);
-        for(auto& e : edges){
+        for(auto& e: edges){
             int u = e[0], v = e[1];
             graph[v].push_back(u);
         }
-        
+
         vector<vector<int>> ans(n);
-        
-        // 2. 对每个节点BFS, 找祖先
+
         for(int i = 0 ; i < n ; i++){
             vector<bool> visited(n, false);
-            queue<int> q;
-            q.push(i);
-            visited[i] = true;
-
-            vector<int> ancestors;
-            while(!q.empty()){
-                auto curr = q.front(); q.pop();
-                for(auto& prev : graph[curr]){
-                    if(!visited[prev]){
-                        visited[prev] = true;
-                        q.push(prev);
-                        ancestors.push_back(prev);
-                    }
-                }
-            }
-            sort(ancestors.begin(), ancestors.end());
-            ans[i] = ancestors;
+            vector<int> path;
+            dfs(graph, path, visited, i);
+            ranges::sort(path);
+            ans[i] = path;
         }
         return ans;
+        
+    }
+
+    void dfs(vector<vector<int>>& graph, vector<int>& path, vector<bool>& visited, int curr){
+        visited[curr] = true;
+        for(auto& prev : graph[curr]){
+            if(!visited[prev]){
+                path.push_back(prev);
+                dfs(graph, path, visited, prev);
+            }
+        }
     }
 };
