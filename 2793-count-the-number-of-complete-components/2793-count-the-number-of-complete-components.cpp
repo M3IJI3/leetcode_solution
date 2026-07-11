73fc57095bee132f57a5e51a123f4fc38e58eb96
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-11 12:48:27 +0800

    Time: 28 ms (78.66%), Space: 114.8 MB (89.96%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,34 +1,49 @@
 class Solution {
+    vector<int> parent;
+    vector<int> size;
+
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y){
+        int rootx = find(x);
+        int rooty = find(y);
+        if(rootx != rooty){
+            if(size[rootx] < size[rooty]) swap(rootx, rooty);
+            parent[rooty] = rootx;
+            size[rootx] = rooty;
+        }
+    }
+
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> graph(n);
-        for(auto& e : edges){
-            int u = e[0], v = e[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        parent.resize(n);
+        size.resize(n, 1);
+
+        for(int i = 0 ; i < n ; i++) parent[i] = i; // 初始化
+        for(auto& e : edges) unite(e[0], e[1]);     // 合并
+
+        vector<int> vertexCount(n, 0);
+        vector<int> edgeCount(n, 0);
+        // 统计每个分量的节点数和边数
+        for(int i = 0 ; i < n ; i++){
+            vertexCount[find(i)]++;
         }
 
-        vector<bool> visited(n, false);
+        for(auto& e : edges){
+            edgeCount[find(e[0])]++;
+        }
 
         int ans = 0;
         for(int i = 0 ; i < n ; i++){
-            if(!visited[i]){
-                queue<int> q;
-                q.push(i);
-                visited[i] = true;
-
-                int v = 0, e = 0;
-                while(!q.empty()){
-                    auto curr = q.front(); q.pop();
-                    v++;
-                    e += graph[curr].size();
-                    for(auto& next : graph[curr]){
-                        if(visited[next]) continue;
-                        q.push(next);
-                        visited[next] = true;
-                    }
-                }
-                if(e == v * (v - 1)) ans++;
+            if(find(i) == i){
+                int v = vertexCount[i];
+                int e = edgeCount[i];
+                if(e == v * (v - 1) / 2) ans++;
             }
         }
         return ans;
