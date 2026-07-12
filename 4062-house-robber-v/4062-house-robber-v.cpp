cabfd2d8d99810b85066f061b48002a96e2197b0
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 18:41:24 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 4062-house-robber-v/4062-house-robber-v.cpp
+++ 4062-house-robber-v/4062-house-robber-v.cpp
@@ -6,17 +6,21 @@ public:
         if(n == 1) return nums[0];
 
         vector<vector<long long>> dp(n, vector<long long>(2, 0));
-        dp[0][0] = 0; dp[0][1] = nums[0];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
 
         for(int i = 1 ; i < n ; i++){
+            // not rob current 
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
 
+            // rob currrent
             if(colors[i] == colors[i - 1]){
                 dp[i][1] = dp[i - 1][0] + nums[i];
             } else {
-                dp[i][1] = max(dp[i - 1][0], dp[i - 1][1]) + nums[i];
+                dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + nums[i]);
             }
         }
         return max(dp[n - 1][0], dp[n - 1][1]);
+        
     }
 };
