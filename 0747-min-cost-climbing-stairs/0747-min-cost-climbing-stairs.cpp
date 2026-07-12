d726906b1af1e2bd07bce88d1be27a9eb48d113d
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 12:43:11 +0800

    Time: 0 ms (100%), Space: 17.1 MB (90.32%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> f(n + 1);
-        for(int i = 2 ; i <= n ; i++){
-            f[i] = min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+        int f0 = 0, f1 = 0;
+        for(int i = 1 ; i < cost.size() ; i++){
+            int new_f = min(f1 + cost[i], f0 + cost[i - 1]);
+            f0 = f1;
+            f1 = new_f;
         }
-        return f[n];
+        return f1;
     }
 };
