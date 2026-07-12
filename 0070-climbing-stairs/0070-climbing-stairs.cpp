70e0100de5e6fd8cd9c2396046632970bb29e3ed
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 12:28:49 +0800

    Time: 0 ms (100%), Space: 7.8 MB (63.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
-    vector<int> memo;
 public:
     int climbStairs(int n) {
-        memo.resize(n + 1);
-        return dfs(n);
-    }
-
-    int dfs(int i){
-        if(i <= 1) return 1;
-        int& res = memo[i];
-        if(res) return res;
-        return res = dfs(i - 1) + dfs(i - 2);
+        int f0 = 1, f1 = 1;
+        for(int i = 2 ; i <= n ; i++){
+            int new_f = f1 + f0;
+            f0 = f1;
+            f1 = new_f;
+        }
+        return f1;
     }
 };
