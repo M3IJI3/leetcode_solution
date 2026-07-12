fd81cbffcd1909c8bd61e9dd4010168a077a810a
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 13:01:43 +0800

    Time: 11 ms (75.96%), Space: 173.5 MB (66.76%) - LeetHub

--- 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n, vector<int>& costs) {
-        vector<int> memo(n + 1);
-        
-        auto dfs = [&](this auto&& dfs, int i) -> int{
-            if(i == 0) return 0;
-            int &res = memo[i];
-            if(res) return res;
-            res = INT_MAX;
-            for(int j = max(i-3, 0); j < i ; j++){
-                res = min(res, dfs(j) + (i - j) * (i - j));
+        vector<int> f(n + 1);
+        for(int i = 1 ; i <= n ; i++){
+            int res = INT_MAX;
+            for(int j = max(i - 3, 0) ; j < i ; j++){
+                res = min(res, f[j] + (i - j) * (i - j));
             }
-            res += costs[i - 1];
-            return res;
-        };
-        return dfs(n);
+            f[i] = res + costs[i - 1];
+        }
+        return f[n];
     }
 };
