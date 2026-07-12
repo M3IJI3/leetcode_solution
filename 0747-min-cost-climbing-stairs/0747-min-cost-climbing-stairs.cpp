85acc59417cd9b30bcb58420f3f6b2b1b004fc30
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 12:42:10 +0800

    Time: 0 ms (100%), Space: 17.2 MB (83.56%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> memo(n + 1, -1);
-        auto dfs = [&](this auto&& dfs, int i) -> int {
-            if(i <= 1) return 0;
-            int &res = memo[i];
-            if(res != -1) return res;
-            return res = min(dfs(i - 1) + cost[i - 1], dfs(i - 2) + cost[i - 2]);
-        };
-        return dfs(n);
+        vector<int> f(n + 1);
+        for(int i = 2 ; i <= n ; i++){
+            f[i] = min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+        }
+        return f[n];
     }
 };
