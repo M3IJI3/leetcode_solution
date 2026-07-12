1d3b1440358a9b237d635c4b020551dc60189cd9
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 18:42:56 +0800

    Time: 199 ms (5.13%), Space: 295.3 MB (5.14%) - LeetHub

--- 4062-house-robber-v/4062-house-robber-v.cpp
+++ 4062-house-robber-v/4062-house-robber-v.cpp
@@ -17,10 +17,10 @@ public:
             if(colors[i] == colors[i - 1]){
                 dp[i][1] = dp[i - 1][0] + nums[i];
             } else {
-                dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + nums[i]);
+                dp[i][1] = max(dp[i - 1][0], dp[i - 1][1]) + nums[i];
             }
         }
         return max(dp[n - 1][0], dp[n - 1][1]);
-        
+
     }
 };
