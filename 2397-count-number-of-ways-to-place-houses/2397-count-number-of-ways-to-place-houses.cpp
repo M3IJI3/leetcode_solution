92c95afeeac534a3adb5bb22c078f19fdec068f3
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 17:28:11 +0800

    Time: 3 ms (78.52%), Space: 7.8 MB (85.36%) - LeetHub

---
+++ 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    int countHousePlacements(int n) {
+        if(n == 1) return 4;
+        if(n == 2) return 9;
+        
+        long long f0 = 1, f1 = 2;
+        for(int i = 2 ; i <= n ; i++){
+            long long new_f = (f0 + f1) % MOD;
+            f0 = f1;
+            f1 = new_f;
+        }
+        return (f1 * f1) % MOD;
+    }
+};
+
+// 1 
+// n
+// n
+// n * n
