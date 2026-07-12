1cb22126ae4f6b53a28278c5c819f21763db3415
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 18:57:10 +0800

    Time: 3 ms (83.37%), Space: 229.4 MB (98.15%) - LeetHub

--- 4062-house-robber-v/4062-house-robber-v.cpp
+++ 4062-house-robber-v/4062-house-robber-v.cpp
@@ -5,22 +5,24 @@ public:
         if(n == 0) return 0;
         if(n == 1) return nums[0];
 
-        long long notRobCurrent = 0;
-        long long robCurrent = nums[0];
+        long long notRob = 0;
+        long long rob = nums[0];
 
         for(int i = 1 ; i < n ; i++){
-            long long newNotRob = max(notRobCurrent, robCurrent);
+            // 当前不偷
+            long long newNotRob = max(notRob, rob);
 
+            // 当前偷
             long long newRob;
             if(colors[i] == colors[i - 1]){
-                newRob = notRobCurrent + nums[i];
+                newRob = notRob + nums[i]; 
             } else {
-                newRob = max(notRobCurrent, robCurrent) + nums[i];
+                newRob = max(notRob, rob) + nums[i];
             }
 
-            notRobCurrent = newNotRob;
-            robCurrent = newRob;
+            notRob = newNotRob;
+            rob = newRob;
         }
-        return max(notRobCurrent, robCurrent);
+        return max(notRob, rob);
     }
 };
