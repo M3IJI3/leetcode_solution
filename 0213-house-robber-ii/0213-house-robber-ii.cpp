3e491bfc0d6eb7ca26c71d0b015359cb8a3fc157
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 17:11:11 +0800

    Time: 0 ms (100%), Space: 10 MB (85.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        return max(nums[0] + linearRob(nums, 2, n - 1), linearRob(nums, 1, n));
+        return max(nums[0] + rob1(nums, 2, n - 1), rob1(nums, 1, n));
     }
 
-    int linearRob(vector<int>& nums, int start, int end){
+    int rob1(vector<int>& nums, int start, int end){
         int f0 = 0, f1 = 0;
         for(int i = start ; i < end ; i++){
             int new_f = max(f1, f0 + nums[i]);
