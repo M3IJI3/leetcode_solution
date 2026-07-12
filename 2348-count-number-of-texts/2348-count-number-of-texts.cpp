81abec61d48b1a13aa37df6d1e5451b6d7d12462
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 16:38:18 +0800

    Time: 31 ms (32.29%), Space: 29.5 MB (21.69%) - LeetHub

--- 2348-count-number-of-texts/2348-count-number-of-texts.cpp
+++ 2348-count-number-of-texts/2348-count-number-of-texts.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
-    int countTexts(string pressedKeys) { 
+    int countTexts(string pressedKeys) {
         int n = pressedKeys.size();
-        vector<long long> f(max(n, 4) + 1, 0);
-        vector<long long> g(max(n, 4) + 1, 0);
+        vector<long long> f(max(n, 4) + 1, 0), g(max(n, 4) + 1, 0);
         f[0] = g[0] = 1;
         f[1] = g[1] = 1;
         f[2] = g[2] = 2;
@@ -13,7 +12,7 @@ public:
         for(int i = 4 ; i <= n ; i++){
             f[i] = (f[i - 1] + f[i - 2] + f[i - 3]) % MOD;
             g[i] = (g[i - 1] + g[i - 2] + g[i - 3] + g[i - 4]) % MOD;
-        }
+        } 
 
         long long ans = 1;
         for(int i = 0 ; i < n;){
@@ -22,9 +21,8 @@ public:
                 j++;
             }
             int len = j - i;
-
             if(pressedKeys[i] == '7' || pressedKeys[i] == '9'){
-                ans = (ans * g[len]) % MOD; 
+                ans = (ans * g[len]) % MOD;
             } else {
                 ans = (ans * f[len]) % MOD;
             }
