80d82bed7b4a5408f7d8130ee8b955872e721c6a
Jie Mei <34743976+M3IJI3@users.noreply.github.com>
2026-07-12 11:28:56 +0800

    Time: 56 ms (57.84%), Space: 24.7 MB (16.18%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
-        int ans = 0;
-        
+        vector<vector<int>> graph(n);
         for(int i = 0 ; i < n ; i++){
+            long long x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2];
+            for(int j = 0 ; j < n ; j++){
+                if(i == j) continue;
+                long long x2 = bombs[j][0], y2 = bombs[j][1], r2 = bombs[j][2];
+                long long dist = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+                if(dist <= r1 * r1){
+                    graph[i].push_back(j);
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            queue<int> q;
             vector<bool> visited(n, false);
-            queue<tuple<int, int, int>> q;
-            int x = bombs[i][0], y = bombs[i][1], r = bombs[i][2];
-            q.push({x, y, r});
+            q.push(i);
             visited[i] = true;
             int explode = 1;
-            
             while(!q.empty()){
-                auto [cx, cy, cr] = q.front(); q.pop();  // 解包当前炸弹
-                for(int j = 0 ; j < n ; j++){
-                    if(!visited[j]){
-                        long long nx = bombs[j][0], ny = bombs[j][1];
-                        long long dist = (nx-cx)*(nx-cx) + (ny-cy)*(ny-cy);
-                        if(dist <= (long long)cr * cr){  // 用当前炸弹的半径
-                            explode++;
-                            q.push({bombs[j][0], bombs[j][1], bombs[j][2]});
-                            visited[j] = true;
-                        }
+                auto curr = q.front(); q.pop();
+                for(auto& next : graph[curr]){
+                    if(!visited[next]){
+                        explode++;
+                        visited[next] = true;
+                        q.push(next);
                     }
                 }
             }
